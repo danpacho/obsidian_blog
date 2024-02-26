cfc8c21437705d260f2e98d770a517564ab67569
Danpa_cho <danpa725@cau.ac.kr>
2024-02-26 21:26:20 +0900

    feat: init astro-blog template with base config

---
+++ packages/astro_blog/astro.config.mjs
@@ -0,0 +1,21 @@
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config'
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://example.com',
+    markdown: {
+        gfm: true,
+        rehypePlugins: [],
+        remarkPlugins: [],
+        remarkRehype: {},
+        shikiConfig: {
+            theme: 'one-dark-pro',
+        },
+        syntaxHighlight: 'shiki',
+    },
+    image: {},
+    vite: {},
+    integrations: [mdx(), sitemap()],
+})
