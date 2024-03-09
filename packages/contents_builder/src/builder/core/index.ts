f8fc9bfa93ee258c635e0ccf43713bfbd34e61ed
Danpa_cho <danpa725@cau.ac.kr>
2024-03-09 15:40:12 +0900

    refactor: core plugin

--- packages/contents_builder/src/builder/core/index.ts
+++ packages/contents_builder/src/builder/core/index.ts
@@ -1,9 +1,9 @@
 import type { UsePlugin } from '../plugin'
 import { ObsidianReference } from './contents/obsidian.reference'
 import { FileTreeConstructor } from './tree/file.tree.constructor'
-import { StaticParamBuilder } from './tree/static.param.builder'
+// import { StaticParamBuilder } from './tree/static.param.builder'
 
 export const CorePlugins = {
-    'build:file:tree': [FileTreeConstructor, StaticParamBuilder],
+    'build:file:tree': [FileTreeConstructor()],
     'build:contents': [ObsidianReference],
 } as const satisfies UsePlugin
