eb5860a3024b5c617a85f0769530a7d9938d7fb3
Danpa_cho <danpa725@cau.ac.kr>
2024-03-09 15:38:40 +0900

    refactor: file tree constructor core plugin

--- packages/contents_builder/src/builder/core/tree/file.tree.constructor.ts
+++ packages/contents_builder/src/builder/core/tree/file.tree.constructor.ts
@@ -1,113 +1,171 @@
-import type { FTreeNode, NodeType } from '../../../parser/node'
-import type { Promisify, Stateful } from '../../../utils/promisify'
-import type { FileTreePlugin } from '../../plugin'
+// import { t } from '@metal-box/type'
+import type { BuildFolderType, FTreeNode, NodeType } from '../../../parser/node'
+import type { Promisify } from '../../../utils/promisify'
+import type { BuilderPlugin } from '../../plugin'
 import type { BuildReport } from '../../reporter'
 
-export const FileTreeConstructor: FileTreePlugin = async ({
-    ast,
-    ioManager,
-    uuidEncoder,
-    buildPath,
-    logger,
-}) => {
-    const rootPath = ast.absolutePath
-
-    const analyzeFileName = (
-        folderName: string
-    ): {
-        type: Exclude<FTreeNode['buildInfo']['folderType'], undefined>
-        value: string
-    } => {
-        if (folderName.match(/^\[(.*?)\]$/)) {
-            const matchedValue = folderName.slice(1, -1)
-            return {
-                type: 'route',
-                value: matchedValue,
-            }
-        } else if (folderName.startsWith('@')) {
-            const matchedValue = folderName.substring(1)
-            return {
-                type: 'series',
-                value: matchedValue,
-            }
-        } else {
-            return {
-                type: 'group',
-                value: folderName,
-            }
-        }
+type TreeConstructorOptions = Readonly<{
+    skipFolderTypes?: Array<BuildFolderType>
+}>
+interface FileTreeConstructorOptions {
+    contents?: TreeConstructorOptions
+    assets?: TreeConstructorOptions
+}
+export const FileTreeConstructor = (
+    options: FileTreeConstructorOptions = {
+        assets: {
+            skipFolderTypes: [],
+        },
+        contents: {
+            skipFolderTypes: ['root'],
+        },
     }
+): BuilderPlugin['build:file:tree'] => {
+    return async ({
+        ast,
+        ioManager,
+        uuidEncoder,
+        buildPath,
+        logger,
+        // metaEngine,
+    }) => {
+        // const engine = metaEngine({
+        //     generator(meta) {
+        //         return {
+        //             title: 'DEFAULT_TITLE',
+        //             date: new Date(),
+        //             description: 'DEFAULT_DESCRIPTION',
+        //             tags: [],
+        //             ...meta,
+        //         }
+        //     },
+        //     parser: t.object({
+        //         title: t.string,
+        //         description: t.string,
+        //         date: t.date,
+        //         tags: t.array(t.string),
+        //     }).parse,
+        // })
 
-    const getBuildRouteInfo = (
-        node: FTreeNode,
-        rootPath: string
-    ): Stateful<
-        {
-            buildPath: string
-        } & ReturnType<typeof analyzeFileName>
-    > => {
-        const absPath = node.absolutePath
-        const originPath = absPath.replace(rootPath, '')
+        const rootPath = ast.absolutePath
 
-        const buildFolderPath = originPath
-            .split('/')
-            .filter(Boolean)
-            .slice(0, -1)
-            .reduce<string>((buildPath, e) => {
-                const { type, value } = analyzeFileName(e)
-                switch (type) {
-                    case 'route': {
-                        return `${buildPath}/${value}`
-                    }
-                    case 'group': {
-                        return `${buildPath}/${value}`
-                    }
-                    case 'series': {
-                        return buildPath
-                    }
+        const analyzeFileName = (
+            folderName?: string
+        ): {
+            type: Exclude<FTreeNode['buildInfo']['folderType'], undefined>
+            value: string
+        } => {
+            if (!folderName) {
+                return {
+                    type: 'root',
+                    value: '',
                 }
-            }, '')
-
-        const folderName =
-            node.category === 'FOLDER'
-                ? originPath.split('/').filter(Boolean).pop()!
-                : originPath
-                      .replace(node.fileName, '')
-                      .split('/')
-                      .filter(Boolean)
-                      .pop()!
-
-        if (!folderName) {
-            return {
-                success: false,
-                error: new Error('Folder name not found'),
+            }
+            if (folderName.match(/^\[(.*?)\]$/)) {
+                const matchedValue = folderName.slice(1, -1)
+                return {
+                    type: 'route',
+                    value: matchedValue,
+                }
+            } else if (folderName.startsWith('@')) {
+                const matchedValue = folderName.substring(1)
+                return {
+                    type: 'series',
+                    value: matchedValue,
+                }
+            } else {
+                return {
+                    type: 'group',
+                    value: folderName,
+                }
             }
         }
 
-        const folderType = analyzeFileName(folderName)
+        const getBuildRouteInfo = (
+            node: FTreeNode,
+            rootPath: string
+        ): {
+            buildPath: string
+        } & ReturnType<typeof analyzeFileName> => {
+            const absPath = node.absolutePath
+            const originPath = absPath.replace(rootPath, '')
 
-        return {
-            success: true,
-            data: { buildPath: buildFolderPath, ...folderType },
+            const buildFolderPath = originPath
+                .split('/')
+                .filter(Boolean)
+                .slice(0, -1)
+                .reduce<string>((buildPath, e) => {
+                    const { type, value } = analyzeFileName(e)
+                    switch (type) {
+                        case 'route': {
+                            return `${buildPath}/${value}`
+                        }
+                        case 'group': {
+                            return `${buildPath}/${value}`
+                        }
+                        case 'series': {
+                            return buildPath
+                        }
+                        case 'root': {
+                            return buildPath
+                        }
+                    }
+                }, '')
+
+            const folderName =
+                node.category === 'FOLDER'
+                    ? originPath.split('/').filter(Boolean).pop()!
+                    : originPath
+                          .replace(node.fileName, '')
+                          .split('/')
+                          .filter(Boolean)
+                          .pop()
+
+            const folderType = analyzeFileName(folderName)
+
+            return { buildPath: buildFolderPath, ...folderType }
         }
-    }
 
-    const getPostBuildInfo = async (
-        node: FTreeNode,
-        buildRoute: ReturnType<typeof getBuildRouteInfo>,
-        buildPath: string
-    ): Promisify<
-        Pick<BuildReport, 'buildID' | 'path'> & {
-            category: NodeType
+        const getAssetBuildInfo = async (
+            node: FTreeNode,
+            buildPath: string
+        ): Promisify<
+            Pick<BuildReport, 'buildID' | 'path'> & {
+                category: NodeType
+            }
+        > => {
+            const assetPrefix: string =
+                node.category === 'IMAGE_FILE'
+                    ? 'images'
+                    : node.category === 'AUDIO_FILE'
+                      ? 'audio'
+                      : 'unknown'
+
+            const buildID = uuidEncoder(node.absolutePath)
+            const assetBuildPath: string = `${buildPath}/${assetPrefix}/${buildID}.${node.fileExtension}`
+
+            return {
+                success: true,
+                data: {
+                    buildID,
+                    path: {
+                        build: assetBuildPath,
+                        origin: node.absolutePath,
+                    },
+                    category: node.category,
+                },
+            }
         }
-    > => {
-        if (!buildRoute.success) return buildRoute
-        const FOLDER_PREFIX = {
-            CONTENTS: 'contents',
-            ASSETS: 'assets',
-        } as const
 
-        if (node.category === 'TEXT_FILE') {
+        const getContentsBuildInfo = async (
+            node: FTreeNode,
+            buildRoute: ReturnType<typeof getBuildRouteInfo>,
+            buildPath: string
+        ): Promisify<
+            Pick<BuildReport, 'buildID' | 'path'> & {
+                category: NodeType
+            }
+        > => {
             const textFile = await ioManager.reader.readFile(node.absolutePath)
             if (!textFile.success) {
                 return {
@@ -116,7 +174,7 @@ export const FileTreeConstructor: FileTreePlugin = async ({
                 }
             }
             const buildID = uuidEncoder(`${textFile.data}${node.absolutePath}`)
-            const postBuildPath: string = `${buildPath}/${FOLDER_PREFIX.CONTENTS}${buildRoute.data.buildPath}/${node.fileName}`
+            const postBuildPath: string = `${buildPath}${buildRoute.buildPath}/${node.fileName}`
             return {
                 success: true,
                 data: {
@@ -130,54 +188,67 @@ export const FileTreeConstructor: FileTreePlugin = async ({
             }
         }
 
-        const assetPrefix: string =
-            node.category === 'IMAGE_FILE'
-                ? 'images'
-                : node.category === 'AUDIO_FILE'
-                  ? 'audio'
-                  : 'unknown'
+        return async (node) => {
+            const buildRouteInfo = getBuildRouteInfo(node, rootPath)
+            if (node.category === 'TEXT_FILE') {
+                if (
+                    options.contents?.skipFolderTypes?.includes(
+                        buildRouteInfo.type
+                    )
+                ) {
+                    logger.info(
+                        `File ${logger.c.underline(node.fileName)} is skipped, invalid file location. Located at root folder`
+                    )
+                    return
+                }
 
-        const buildID = uuidEncoder(node.absolutePath)
-        const assetBuildPath: string = `${buildPath}/${FOLDER_PREFIX.ASSETS}/${assetPrefix}/${buildID}.${node.fileExtension}`
+                const postBuildInfo = await getContentsBuildInfo(
+                    node,
+                    buildRouteInfo,
+                    buildPath.content
+                )
 
-        return {
-            success: true,
-            data: {
-                buildID,
-                path: {
-                    build: assetBuildPath,
-                    origin: node.absolutePath,
-                },
-                category: node.category,
-            },
+                if (!postBuildInfo.success) {
+                    logger.error(
+                        `File ${logger.c.underline(node.fileName)} is rejected due to read error`
+                    )
+                    return
+                }
+
+                node.updateBuildInfo({
+                    id: postBuildInfo.data.buildID,
+                    path: postBuildInfo.data.path.build,
+                    folderType: buildRouteInfo.type,
+                })
+            } else {
+                if (
+                    options.assets?.skipFolderTypes?.includes(
+                        buildRouteInfo.type
+                    )
+                ) {
+                    logger.info(
+                        `File ${logger.c.underline(node.fileName)} is skipped, invalid file location. Located at root folder`
+                    )
+                    return
+                }
+
+                const assetBuildInfo = await getAssetBuildInfo(
+                    node,
+                    buildPath.assets
+                )
+                if (!assetBuildInfo.success) {
+                    logger.error(
+                        `File ${logger.c.underline(node.fileName)} is rejected due to read error`
+                    )
+                    return
+                }
+
+                node.updateBuildInfo({
+                    id: assetBuildInfo.data.buildID,
+                    path: assetBuildInfo.data.path.build,
+                    folderType: buildRouteInfo.type,
+                })
+            }
         }
-    }
-
-    return async (node) => {
-        const buildRouteInfo = getBuildRouteInfo(node, rootPath)
-
-        if (!buildRouteInfo.success) {
-            logger.error(
-                `File ${logger.c.underline(node.fileName)} is skipped, invalid file location.`
-            )
-            return
-        }
-
-        const postBuildInfo = await getPostBuildInfo(
-            node,
-            buildRouteInfo,
-            buildPath
-        )
-
-        if (!postBuildInfo.success) {
-            logger.error(
-                `File ${logger.c.underline(node.fileName)} is rejected due to read error`
-            )
-            return
-        }
-
-        node.buildInfo.id = postBuildInfo.data.buildID
-        node.buildInfo.path = postBuildInfo.data.path.build
-        node.buildInfo.folderType = buildRouteInfo.data.type
     }
 }
