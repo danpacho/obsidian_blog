7242164c75d7cff72ac516e1ce59f488d4118d4d
Danpa_cho <danpa725@cau.ac.kr>
2024-03-10 00:35:00 +0900

    feat: register added plugins

--- packages/contents_builder/src/builder/core/index.ts
+++ packages/contents_builder/src/builder/core/index.ts
@@ -1,9 +1,14 @@
-import type { UsePlugin } from '../plugin'
+import type { PluginAdapter } from '../plugin'
 import { ObsidianReference } from './contents/obsidian.reference'
 import { FileTreeConstructor } from './tree/file.tree.constructor'
-// import { StaticParamBuilder } from './tree/static.param.builder'
+import { MetaBuilder } from './tree/meta.builder'
+import { StaticParamBuilder } from './tree/static.param.builder'
 
 export const CorePlugins = {
-    'build:file:tree': [FileTreeConstructor()],
+    'build:file:tree': [
+        FileTreeConstructor(),
+        MetaBuilder(),
+        StaticParamBuilder(),
+    ],
     'build:contents': [ObsidianReference],
-} as const satisfies UsePlugin
+} as const satisfies PluginAdapter
