ab4013c7ca0102fc928abca14bdff510f130b9da
Danpa_cho <danpa725@cau.ac.kr>
2024-03-11 12:05:38 +0900

    feat: add pagination interface at meta constant

--- packages/contents_builder/src/builder/core/tree/shared/meta/constant.ts
+++ packages/contents_builder/src/builder/core/tree/shared/meta/constant.ts
@@ -1,21 +1,36 @@
-export const MetaField: {
-    required: readonly string[]
-    optional: readonly string[]
-} = {
+export const ContentMetaField = {
     required: ['title', 'description'],
     optional: ['update', 'category', 'tags'],
+    build: ['params', 'pagination'],
+} as const
+
+type UndefinableString = string | undefined
+export interface DefaultPaginationInfo {
+    href: UndefinableString
+    title: UndefinableString
+    description: UndefinableString
 }
 
-export interface DefaultMeta {
+export interface DefaultContentMeta {
     title: string
     description: string
     update?: Date
     category?: string
     tags?: Array<string>
+    params?: Record<string, string>
+    pagination?: {
+        prev: DefaultPaginationInfo
+        next: DefaultPaginationInfo
+    }
 }
 
-export const MetaRequiredDefaultValue = () => ({
+export const ContentMetaDefaultValueInjector = () => ({
     title: 'DEFAULT TITLE',
     description: 'DEFAULT DESCRIPTION',
     update: new Date(),
 })
+
+export interface DefaultCategoryMeta {
+    title: string
+    description: string
+}
