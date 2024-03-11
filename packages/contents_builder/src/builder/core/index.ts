d7cf3dfefbefa0f1444eee4ff591a9abe0770286
Danpa_cho <danpa725@cau.ac.kr>
2024-03-11 12:09:45 +0900

    feat: register `PaginationBuilder` plugin

--- packages/contents_builder/src/builder/core/index.ts
+++ packages/contents_builder/src/builder/core/index.ts
@@ -2,13 +2,14 @@ import type { PluginAdapter } from '../plugin'
 import { ObsidianReference } from './contents/obsidian.reference'
 import { FileTreeConstructor } from './tree/file.tree.constructor'
 import { MetaBuilder } from './tree/meta.builder'
+import { PaginationBuilder } from './tree/pagination.builder'
 import { StaticParamBuilder } from './tree/static.param.builder'
-
 export const CorePlugins = {
-    'build:file:tree': [
+    'build:origin:tree': [
         FileTreeConstructor(),
         MetaBuilder(),
         StaticParamBuilder(),
     ],
+    'build:generated:tree': [PaginationBuilder()],
     'build:contents': [ObsidianReference],
 } as const satisfies PluginAdapter
