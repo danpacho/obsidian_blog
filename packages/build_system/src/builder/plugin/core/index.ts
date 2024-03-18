4d78433c620466099d9e9aa1671fb2f41cddf3ee
Danpa_cho <danpa725@cau.ac.kr>
2024-03-18 18:46:49 +0900

    feat: add new core plugins and configs

--- packages/build_system/src/builder/plugin/core/index.ts
+++ packages/build_system/src/builder/plugin/core/index.ts
@@ -1,28 +1,43 @@
 import type { PluginAdapter } from '..'
 import { ObsidianReference } from './contents/obsidian.reference'
-import { CategoryDescriptionGenerator } from './tree/category.description.generator'
+import {
+    CategoryDescriptionGenerator,
+    type CategoryDescriptionGeneratorConfig,
+} from './tree/category.description.generator'
 import { MetaBuilder, type MetaBuilderConfig } from './tree/meta.builder'
+import { MetaValidator, MetaValidatorConfig } from './tree/meta.validator'
 import {
     PaginationBuilder,
     type PaginationBuilderConfig,
 } from './tree/pagination.builder'
 import {
+    SeriesInfoGenerator,
+    type SeriesInfoGeneratorConfig,
+} from './tree/series.info.generator'
+import {
     StaticParamBuilder,
-    type StaticParamBuilderOptions,
+    type StaticParamBuilderConfig,
 } from './tree/static.param.builder'
 
 export interface CorePluginsConfig {
+    metaValidator?: MetaValidatorConfig
     paginationBuilder?: PaginationBuilderConfig
     metaBuilder?: MetaBuilderConfig
-    staticParamBuilder?: StaticParamBuilderOptions
+    staticParamBuilder?: StaticParamBuilderConfig
+    seriesInfoGenerator?: SeriesInfoGeneratorConfig
+    categoryDescriptionGenerator?: CategoryDescriptionGeneratorConfig
 }
 export const CorePlugins = (coreConfig?: CorePluginsConfig) => {
     return {
         'walk:generated:tree': [
+            MetaValidator(coreConfig?.metaValidator),
             MetaBuilder(coreConfig?.metaBuilder),
             StaticParamBuilder(coreConfig?.staticParamBuilder),
             PaginationBuilder(coreConfig?.paginationBuilder),
-            CategoryDescriptionGenerator(),
+            SeriesInfoGenerator(coreConfig?.seriesInfoGenerator),
+            CategoryDescriptionGenerator(
+                coreConfig?.categoryDescriptionGenerator
+            ),
         ],
         'build:contents': [ObsidianReference()],
     } as const satisfies PluginAdapter
