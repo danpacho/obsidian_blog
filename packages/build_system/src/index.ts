0bb7a299fc7bbdf76e54e4f8c2766bac722375eb
Danpa_cho <danpa725@cau.ac.kr>
2024-03-28 23:56:40 +0900

    chore: export IOManager and plugin interface at build_system

--- packages/build_system/src/index.ts
+++ packages/build_system/src/index.ts
@@ -1,1 +1,6 @@
 export { System as BuildSystem } from './system'
+export { IOManager } from './io_manager'
+export {
+    type FileTreePluginConfig,
+    type ContentsModifierPluginConfig,
+} from './builder/plugin/interface'
