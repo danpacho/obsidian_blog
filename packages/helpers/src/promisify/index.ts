530f9b65c7b6fe6014446f571eb543f319f22e48
Danpa_cho <danpa725@cau.ac.kr>
2024-05-06 22:12:36 +0900

    move: promisify util types into @blogger/helpers

---
+++ packages/helpers/src/promisify/index.ts
@@ -0,0 +1,19 @@
+export type PromiseCallbacks<DataInterface, ErrorInterface extends Error> =
+    | {
+          onSuccess?: (data: DataInterface) => void
+          onError?: (err: ErrorInterface) => void
+      }
+    | undefined
+
+export type Stateful<DataInterface, ErrorInterface = unknown> =
+    | {
+          success: true
+          data: DataInterface
+      }
+    | {
+          success: false
+          error: ErrorInterface
+      }
+export type Promisify<DataInterface, ErrorInterface = unknown> = Promise<
+    Stateful<DataInterface, ErrorInterface>
+>
