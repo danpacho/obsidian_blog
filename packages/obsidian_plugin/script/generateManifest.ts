65f60a3d41acd57f0469c532c51e8d8d52b2d193
Danpa_cho <danpa725@cau.ac.kr>
2024-07-01 19:52:53 +0900

    feat: add hot-reload dev setup and pkg info generator for plugin dev

---
+++ packages/obsidian_plugin/script/generateManifest.ts
@@ -0,0 +1,23 @@
+import { cwd } from 'process'
+import { PluginManifest } from 'obsidian'
+import { io, lg } from '../utils'
+
+export const generateManifest = async (
+    manifest: PluginManifest
+): Promise<void> => {
+    const json = JSON.stringify(manifest, null, 4)
+    await io.writer.write({
+        data: json,
+        filePath: `${cwd()}/manifest.json`,
+        handler: {
+            onSuccess: () => {
+                lg.success(`Manifest file created\n${json}`)
+            },
+            onError: (error) => {
+                lg.error(
+                    `Error creating manifest file\n${JSON.stringify(error, null, 4)}`
+                )
+            },
+        },
+    })
+}
