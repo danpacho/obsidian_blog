7ec7d832fc5d7ee65a4f8f8de20f4a1c0b65ee83
Danpa_cho <danpa725@cau.ac.kr>
2024-07-01 19:42:09 +0900

    refactor: BuildSystem interface, extract publish system related pkg

--- packages/build_system/src/system.ts
+++ packages/build_system/src/system.ts
@@ -1,12 +1,10 @@
-import { IO as IOManager } from '@blogger/helpers'
-import { Logger } from '@blogger/logger'
-import { BuildSystem, type BuildSystemConstructor } from './builder'
-import { FileTreeParser, type FileTreeParserConstructor } from './parser'
-import { GitShell } from './publisher/git'
+import { IO as IOManager, Logger } from '@obsidian_blogger/helpers'
 import {
-    ShellScript,
-    type ShellScriptConstructor,
-} from './publisher/shell.script'
+    BuildSystem,
+    type BuildSystemConstructor,
+    PluginAdapter,
+} from './builder'
+import { FileTreeParser, type FileTreeParserConstructor } from './parser'
 
 //TODO: refactor, divide constructor <-> config options
 type ClassInstance = 'io' | 'logger' | 'parser'
@@ -14,17 +12,12 @@ type ClassInstance = 'io' | 'logger' | 'parser'
 interface SystemConstructor {
     parser: Omit<FileTreeParserConstructor, ClassInstance>
     builder: Omit<BuildSystemConstructor, ClassInstance>
-    shell: Omit<ShellScriptConstructor, ClassInstance>
 }
 export class System {
     private readonly parser: FileTreeParser
-    public readonly builder: BuildSystem
+    private readonly builder: BuildSystem
     private readonly logger: Logger
     private readonly io: IOManager
-    public readonly script: {
-        readonly shell: ShellScript
-        readonly git: GitShell
-    }
 
     public constructor(options: SystemConstructor) {
         this.io = new IOManager()
@@ -41,15 +34,29 @@ export class System {
             logger: this.logger,
             ...options.builder,
         })
-        this.script = {
-            shell: new ShellScript({
-                ...options.shell,
-                logger: this.logger,
-            }),
-            git: new GitShell({
-                ...options.shell,
-                logger: this.logger,
-            }),
-        }
+    }
+
+    /**
+     * @description Build the project
+     */
+    public async build() {
+        await this.builder.build()
+    }
+
+    /**
+     * @description Use a plugin
+     * @param plugins
+     * @example
+     * ```ts
+     * const system = new BuildSystem(...)
+     * system.use({
+     *      "walk:generate:tree": [plugin0],
+     *      "build:origin:tree": [plugin1, plugin2],
+     *      "build:contents": [plugin3, plugin4],
+     * })
+     * ```
+     */
+    public use(plugins: PluginAdapter) {
+        this.builder.use(plugins)
     }
 }
