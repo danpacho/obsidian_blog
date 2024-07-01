9fb5692b4a0ca8503abada0d110d8b2e59c60700
Danpa_cho <danpa725@cau.ac.kr>
2024-07-01 20:15:59 +0900

    feat: add io, logger helper instance

---
+++ packages/obsidian_plugin/utils/index.ts
@@ -0,0 +1,5 @@
+import { IO } from '@obsidian_blogger/helpers/io'
+import { Logger } from '@obsidian_blogger/helpers/logger'
+
+export const io = new IO()
+export const lg = new Logger({ name: 'plugin_builder' })
