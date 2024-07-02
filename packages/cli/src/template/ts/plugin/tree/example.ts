585f737aa4eb51c26415c888cb4ae075d789916f
Danpa_cho <danpa725@cau.ac.kr>
2024-07-02 16:30:37 +0900

    feat: add tree related plugin template

---
+++ packages/cli/src/template/ts/plugin/tree/example.ts
@@ -0,0 +1,62 @@
+import { type BuilderPlugin } from '@obsidian_blogger/build_system'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+// -------------------------------------------------------------- //
+// ------------------------ Tree Plugin ------------------------- //
+// -------------------------------------------------------------- //
+
+interface ExampleTreeWalkerConfig {
+    // Add your configuration here
+    option1: string
+    option2: number
+}
+export const ExampleTreeWaler = (
+    config: ExampleTreeWalkerConfig
+): BuilderPlugin['walk:generated:tree'] => {
+    const { option1, option2 } = config
+
+    return async ({
+        buildPath,
+        io,
+        logger,
+        meta,
+        pathGenerator,
+        disableCorePlugins,
+    }) => {
+        return {
+            name: 'ExampleTreeWaler',
+            disableCache: false,
+            skipFolderNode: true,
+            exclude: [],
+            walker: async (node, i, children) => {},
+        }
+    }
+}
+
+interface ExampleTreeBuilderPluginConfig {
+    // Add your configuration here
+    option1: string
+    option2: number
+}
+export const ExampleTreeBuilder = (
+    config: ExampleTreeBuilderPluginConfig
+): BuilderPlugin['build:origin:tree'] => {
+    const { option1, option2 } = config
+
+    return async ({
+        buildPath,
+        io,
+        logger,
+        meta,
+        pathGenerator,
+        disableCorePlugins,
+    }) => {
+        return {
+            name: 'ExampleTreeBuilder',
+            disableCache: false,
+            skipFolderNode: true,
+            exclude: [],
+            walker: async (node, i, children) => {},
+        }
+    }
+}
