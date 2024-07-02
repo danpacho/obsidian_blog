7536ff530ac75cbf6b8139774abc54c4d1fc56f7
Danpa_cho <danpa725@cau.ac.kr>
2024-07-02 16:29:45 +0900

    chore: add bridge build config

---
+++ packages/cli/src/template/ts/tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig((options) => ({
+    entry: {
+        build: 'src/build.ts',
+        publish: 'src/publish.ts',
+    },
+    watch: options.watch ? ['src/**/*'] : false,
+    clean: true,
+    dts: true,
+    outDir: 'dist',
+    target: 'esnext',
+    format: ['cjs', 'esm'],
+    sourcemap: false,
+}))
