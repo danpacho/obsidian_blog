5937328080b0f631b47b792164cab3ec96cbbd0f
Danpa_cho <danpa725@cau.ac.kr>
2024-07-02 15:08:07 +0900

    refactor: BloggerCLI class

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -5,9 +5,97 @@ import { CLI } from './core/cli.js'
 import { PkgManager } from './core/pkg_manager.js'
 import { GithubRepository } from './core/repo.js'
 
-export class BloggerCLI extends CLI {
+type BloggerCLIOptions = {
+    /**
+     * Log output
+     * @default true
+     */
+    verbose: boolean
+    /**
+     * Use TypeScript
+     * @default true
+     */
+    ts: boolean
+}
+
+export class BloggerCLI extends CLI<BloggerCLIOptions> {
     private readonly $repo: GithubRepository
     private readonly $pkgManager: PkgManager
+
+    private async installBridgePkg(installPath: string): Promise<void> {
+        const { ts: isTs, verbose: log } = this.programOptions
+
+        const repository_path =
+            `https://github.com/danpacho/obsidian_blog/tree/main/packages/cli/src/template/${isTs ? 'ts' : 'js'}` as const
+
+        log &&
+            this.$logger.info(
+                `Fetching repository information... for ${repository_path}`
+            )
+        const repo = new URL(repository_path!)
+        const repoInfo = await this.$repo.getRepoInfo(repo)
+        if (!repoInfo) {
+            this.$logger.error('Repository not found')
+            return
+        }
+        log &&
+            this.$logger.success(
+                `Repository: ${repoInfo.username}/${repoInfo.name}`
+            )
+        log && this.$logger.success(`Branch: ${repoInfo.branch}`)
+
+        try {
+            // TODO: check this part
+            await this.$io.writer.createFolder(installPath)
+
+            await this.$repo.downloadAndExtractRepo(installPath, repoInfo, {
+                onRetry: (e, attempt) => {
+                    this.$logger.log(
+                        `Attempt ${attempt} failed: ${e.message}, retrying...`
+                    )
+                },
+            })
+
+            this.$logger.success(
+                `Repository installed successfully at ${installPath}`
+            )
+            // TODO: remove deleteFolder__FORCE for production code
+            await this.$io.writer.deleteFolder__FORCE(installPath)
+        } catch (e) {
+            this.$logger.error(JSON.stringify(e))
+        }
+    }
+
+    private parseArgOptions(arg: string): string {
+        const isFirstCharEqual = arg.slice(0, 1) === '='
+        if (isFirstCharEqual) {
+            const parsed = arg.replace('=', '')
+            return parsed
+        }
+
+        return arg
+    }
+
+    private parseBooleanArgOptions(arg: string): boolean {
+        const parsedArg = this.parseArgOptions(arg)
+        return parsedArg === 'true' ? true : false
+    }
+
+    private registerOptions(): void {
+        this.addCommand({
+            optFlag: '-v, --verbose [option]',
+            optDescription: 'Log output',
+            optDefaultValue: true,
+            optArgParser: (value) => this.parseBooleanArgOptions(value),
+        })
+        this.addCommand({
+            optFlag: '-t --ts [option]',
+            optDescription: 'Use TypeScript',
+            optDefaultValue: true,
+            optArgParser: (value) => this.parseBooleanArgOptions(value),
+        })
+    }
+
     public constructor() {
         super({
             info: {
@@ -21,46 +109,14 @@ export class BloggerCLI extends CLI {
         this.$pkgManager = new PkgManager()
 
         // Add global options
-        this.addCommand({
-            optFlag: '-v, --verbose [option]',
-            optDescription: 'Log output',
-            optDefaultValue: true,
-            optArgParser: (value, prev) => {
-                value.slice(0, 1) === '=' && (value = value.slice(1)) // Remove '=' if
-                if (value === 'false') {
-                    return false
-                } else return prev
-            },
-        })
+        this.registerOptions()
 
         this.addCommand({
             cmdFlag: 'repo',
-            argFlag: ['<repository_path>', '<install_path>'],
+            argFlag: ['<install_path>'],
             cmdDescription: 'Fetch information about a GitHub repository',
-            cmdAction: async ({ repository_path, install_path }) => {
-                const log: boolean = this.programOptions.verbose
-
-                log &&
-                    this.$logger.info(
-                        `Fetching repository information... for ${JSON.stringify(repository_path)}`
-                    )
-                const repo = new URL(repository_path!)
-                const repoInfo = await this.$repo.getRepoInfo(repo)
-                if (!repoInfo) {
-                    this.$logger.error('Repository not found')
-                    return
-                }
-                log &&
-                    this.$logger.success(
-                        `Repository: ${repoInfo.username}/${repoInfo.name}`
-                    )
-                log && this.$logger.success(`Branch: ${repoInfo.branch}`)
-                await this.$io.writer.createFolder(install_path)
-                await this.$repo.downloadAndExtractRepo(install_path, repoInfo)
-
-                this.$logger.success('Repository installed successfully')
-                // TODO: remove deleteFolder__FORCE for production code
-                await this.$io.writer.deleteFolder__FORCE(install_path)
+            cmdAction: async ({ install_path }) => {
+                await this.installBridgePkg(install_path)
             },
         })
 
@@ -74,9 +130,11 @@ export class BloggerCLI extends CLI {
                 this.$logger.success('Package installed successfully')
             },
         })
+
+        // TODO: write down the file based on the user build options e.g) origin path, build path, etc...
     }
 
-    public async run() {
+    public run() {
         this.$program.parse(process.argv)
     }
 }
