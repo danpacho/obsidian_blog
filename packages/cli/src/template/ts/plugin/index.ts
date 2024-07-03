b44c873bbec5f3f1a670081c478d34e182350df7
Danpa_cho <danpa725@cau.ac.kr>
2024-07-03 21:41:36 +0900

    refactor: rename bridge ts template structure

--- packages/cli/src/template/ts/plugin/index.ts
+++ packages/cli/src/template/ts/plugin/index.ts
@@ -1,6 +1,6 @@
 import * as buildContentsPlugins from './build:contents/index.js'
-import * as buildOriginTreePlugins from './build:origin:tree/index.js'
-import * as walkGeneratedTreePlugins from './walk:generated:tree/index.js'
+import * as buildOriginTreePlugins from './build:tree/index.js'
+import * as walkGeneratedTreePlugins from './walk:tree/index.js'
 
 export default {
     buildContents: buildContentsPlugins,
