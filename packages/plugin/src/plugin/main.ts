4dd0d7dcea6ae31221b66f80c720c18906f7a742
Danpa_cho <danpa725@cau.ac.kr>
2024-07-05 01:19:54 +0900

    feat: connect react view and obsidian

--- packages/plugin/src/plugin/main.ts
+++ packages/plugin/src/plugin/main.ts
@@ -1,15 +1,14 @@
-import { ShellExecutor } from '@obsidian_blogger/helpers/shell'
 import {
     App,
-    Editor,
-    MarkdownView,
-    Modal,
-    Notice,
+    // Modal,
+    // Notice,
     Plugin,
     PluginManifest,
     PluginSettingTab,
     Setting,
+    WorkspaceLeaf,
 } from 'obsidian'
+import { ReactView, VIEW_TYPE } from '~/react/react.mounter'
 
 interface BloggerSettings {
     name: string
@@ -21,6 +20,7 @@ const DEFAULT_SETTINGS = {
 
 export default class ObsidianBlogger extends Plugin {
     public settings: BloggerSettings
+    private isViewOpen: boolean = false
 
     constructor(app: App, manifest: PluginManifest) {
         super(app, manifest)
@@ -29,77 +29,72 @@ export default class ObsidianBlogger extends Plugin {
         }
     }
 
+    private async activateReactView() {
+        const { workspace } = this.app
+
+        let leaf: WorkspaceLeaf | null = null
+        const leaves = workspace.getLeavesOfType(VIEW_TYPE)
+
+        if (leaves.length > 0) {
+            // A leaf with our view already exists, use that
+            leaf = leaves[0]!
+        } else {
+            // Our view could not be found in the workspace, create a new leaf
+            // in the right sidebar for it
+            leaf = workspace.getRightLeaf(false)
+            await leaf!.setViewState({ type: VIEW_TYPE, active: true })
+        }
+
+        // "Reveal" the leaf in case it is in a collapsed sidebar
+        workspace.revealLeaf(leaf!)
+
+        this.isViewOpen = true
+    }
+
+    private async deactivateReactView() {
+        const { workspace } = this.app
+
+        const leaves = workspace.getLeavesOfType(VIEW_TYPE)
+        if (leaves.length === 0) return
+
+        const leaf = leaves[0]!
+        await leaf.setViewState({ type: VIEW_TYPE, active: false })
+        workspace.detachLeavesOfType(VIEW_TYPE)
+        workspace.rightSplit.collapse()
+        this.isViewOpen = false
+    }
+
+    private registerCommands() {
+        this.addCommand({
+            id: 'open-obsidian-blogger',
+            name: 'Open Obsidian Blogger',
+            callback: () => {
+                this.activateReactView()
+            },
+        })
+    }
+
     override async onload() {
         await this.loadSettings()
-
-        // This creates an icon in the left ribbon.
+        this.registerView(VIEW_TYPE, (leaf) => new ReactView(leaf))
+        this.registerCommands()
         const ribbonIconEl = this.addRibbonIcon(
-            'heart',
+            'wrench',
             'Obsidian blogger',
-            (evt: MouseEvent) => {
+            async (evt: MouseEvent) => {
                 console.log('Ribbon icon clicked', evt)
-                // Called when the user clicks the icon.
-                new ObsidianBloggerModal(this.app).open()
+                console.log('isViewOpen', this.isViewOpen)
+                if (!this.isViewOpen) await this.activateReactView()
+                else await this.deactivateReactView()
             }
         )
         // Perform additional things with the ribbon
         ribbonIconEl.addClass('my-plugin-ribbon-class')
 
-        // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
         const statusBar = this.addStatusBarItem()
         statusBar.createEl('span', { text: '💡 updated' })
 
-        // This adds a simple command that can be triggered anywhere
-        this.addCommand({
-            id: 'open-sample-modal-simple',
-            name: 'Obsidian-blogger build',
-            callback: () => {
-                new ObsidianBloggerModal(this.app).open()
-            },
-        })
-        // This adds an editor command that can perform some operation on the current editor instance
-        this.addCommand({
-            id: 'sample-editor-command',
-            name: 'Obsidian blogger (editor)',
-            editorCallback: (editor: Editor) => {
-                console.log(editor.getSelection())
-                editor.replaceSelection('Sample Editor Command')
-            },
-        })
-        // This adds a complex command that can check whether the current state of the app allows execution of the command
-        this.addCommand({
-            id: 'open-sample-modal-complex',
-            name: 'Obsidian blogger (complex)',
-            checkCallback: (checking: boolean) => {
-                // Conditions to check
-                const markdownView =
-                    this.app.workspace.getActiveViewOfType(MarkdownView)
-                if (markdownView) {
-                    // If checking is true, we're simply "checking" if the command can be run.
-                    // If checking is false, then we want to actually perform the operation.
-                    if (!checking) {
-                        new ObsidianBloggerModal(this.app).open()
-                    }
-
-                    // This command will only show up in Command Palette when the check function returns true
-                    return true
-                }
-            },
-        })
-
-        // This adds a settings tab so the user can configure various aspects of the plugin
         this.addSettingTab(new ObsidianBloggerSetting(this.app, this))
-
-        // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-        // Using this function will automatically remove the event listener when this plugin is disabled.
-        // this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-        //     console.log('click', evt)
-        // })
-
-        // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-        // this.registerInterval(
-        //     window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000)
-        // )
     }
 
     override onunload() {}
@@ -117,108 +112,108 @@ export default class ObsidianBlogger extends Plugin {
     }
 }
 
-class ObsidianBloggerModal extends Modal {
-    private result: string | undefined
-    private onSubmit: (result: string) => void
-    private shell: ShellExecutor
+// class ObsidianBloggerModal extends Modal {
+//     private result: string | undefined
+//     private onSubmit: (result: string) => void
+//     private shell: ShellExecutor
 
-    constructor(app: App) {
-        super(app)
-        this.onSubmit = (result: string) => {
-            this.setTitle(result)
-            const modal = new Modal(app)
-            modal.setTitle('Result')
-            modal.setContent(result)
-            modal.open()
-        }
-        this.shell = new ShellExecutor(100)
-    }
+//     constructor(app: App) {
+//         super(app)
+//         this.onSubmit = (result: string) => {
+//             this.setTitle(result)
+//             const modal = new Modal(app)
+//             modal.setTitle('Result')
+//             modal.setContent(result)
+//             modal.open()
+//         }
+//         this.shell = new ShellExecutor(100)
+//     }
 
-    override onOpen() {
-        const { contentEl } = this
+//     override onOpen() {
+//         const { contentEl } = this
 
-        contentEl.createEl('h2', { text: "What's your name??" })
+//         contentEl.createEl('h2', { text: "What's your name??" })
 
-        new Setting(contentEl).setName('Name').addText((text) =>
-            text.onChange((value) => {
-                this.result = value
-            })
-        )
+//         new Setting(contentEl).setName('Name').addText((text) =>
+//             text.onChange((value) => {
+//                 this.result = value
+//             })
+//         )
 
-        new Setting(contentEl).addButton((btn) =>
-            btn
-                .setButtonText('Submit')
-                .setCta()
-                .onClick(() => {
-                    if (!this.result) return
-                    this.close()
-                    this.onSubmit(this.result)
-                })
-        )
+//         new Setting(contentEl).addButton((btn) =>
+//             btn
+//                 .setButtonText('Submit')
+//                 .setCta()
+//                 .onClick(() => {
+//                     if (!this.result) return
+//                     this.close()
+//                     this.onSubmit(this.result)
+//                 })
+//         )
 
-        new Setting(contentEl).addButton((btn) =>
-            btn
-                .setButtonText('Install')
-                .setCta()
-                .onClick(async () => {
-                    try {
-                        try {
-                            const nodePath =
-                                '/Users/june/.nvm/versions/node/v20.11.0/bin/node'
-                            const npxPath =
-                                '/Users/june/.nvm/versions/node/v20.11.0/bin/npx'
+//         new Setting(contentEl).addButton((btn) =>
+//             btn
+//                 .setButtonText('Install')
+//                 .setCta()
+//                 .onClick(async () => {
+//                     try {
+//                         try {
+//                             const nodePath =
+//                                 '/Users/june/.nvm/versions/node/v20.11.0/bin/node'
+//                             const npxPath =
+//                                 '/Users/june/.nvm/versions/node/v20.11.0/bin/npx'
 
-                            const a = await this.shell.spawn$(
-                                nodePath,
-                                [
-                                    npxPath,
-                                    'create-next-app@latest',
-                                    '/Users/june/Documents/project/obsidian_blogger/cli',
-                                    `--use-npm`,
-                                    '--ts',
-                                    '--tailwind',
-                                    '--app',
-                                    '--src-dir',
-                                    '--import-alias="@/*"',
-                                    '--eslint',
-                                ],
-                                {
-                                    env: {
-                                        //!WARN: /Users/june/.nvm/versions/node/v20.11.0/bin should be origin path
-                                        PATH: `${process.env.PATH}:/Users/june/.nvm/versions/node/v20.11.0/bin`,
-                                    },
-                                }
-                            )
+//                             const a = await this.shell.spawn$(
+//                                 nodePath,
+//                                 [
+//                                     npxPath,
+//                                     'create-next-app@latest',
+//                                     '/Users/june/Documents/project/obsidian_blogger/cli',
+//                                     `--use-npm`,
+//                                     '--ts',
+//                                     '--tailwind',
+//                                     '--app',
+//                                     '--src-dir',
+//                                     '--import-alias="@/*"',
+//                                     '--eslint',
+//                                 ],
+//                                 {
+//                                     env: {
+//                                         //!WARN: /Users/june/.nvm/versions/node/v20.11.0/bin should be origin path
+//                                         PATH: `${process.env.PATH}:/Users/june/.nvm/versions/node/v20.11.0/bin`,
+//                                     },
+//                                 }
+//                             )
 
-                            const b = await this.shell.exec$(
-                                'rm -rf /Users/june/Documents/project/obsidian_blogger/cli',
-                                true
-                            )
+//                             const b = await this.shell.exec$(
+//                                 'rm -rf /Users/june/Documents/project/obsidian_blogger/cli',
+//                                 true
+//                             )
 
-                            const c = await this.shell.exec$('ls -al', true)
+//                             const c = await this.shell.exec$('ls -al', true)
 
-                            const res = `${a.result}\n${b.stdout}\n${c.stdout}`
-                            console.log(res)
-                            this.onSubmit(res)
-                            this.result = res ?? 'No result'
-                        } catch (err) {
-                            this.onSubmit(
-                                `Can't execute command ${JSON.stringify(err, null, 4)}`
-                            )
-                            this.result = `Can't execute command ${JSON.stringify(err, null, 4)}`
-                        }
-                    } catch (e) {
-                        new Notice("Can't execute command")
-                    }
-                })
-        )
-    }
+//                             const res = `${a.result}\n${b.stdout}\n${c.stdout}`
+//                             console.log(res)
+//                             this.onSubmit(res)
+//                             this.result = res ?? 'No result'
+//                         } catch (err) {
+//                             this.onSubmit(
+//                                 `Can't execute command ${JSON.stringify(err, null, 4)}`
+//                             )
+//                             this.result = `Can't execute command ${JSON.stringify(err, null, 4)}`
+//                         }
+//                     } catch (e) {
+//                         new Notice("Can't execute command")
+//                     }
+//                 })
+//         )
+//     }
 
-    override onClose() {
-        const { contentEl } = this
-        contentEl.empty()
-    }
-}
+//     override onClose() {
+//         const { contentEl } = this
+//         contentEl.empty()
+//     }
+// }
 
 class ObsidianBloggerSetting extends PluginSettingTab {
     constructor(
@@ -248,27 +243,3 @@ class ObsidianBloggerSetting extends PluginSettingTab {
             )
     }
 }
-
-// export class ExampleView extends ItemView {
-//     constructor(leaf: WorkspaceLeaf) {
-//         super(leaf)
-//     }
-
-//     getViewType() {
-//         return VIEW_TYPE_EXAMPLE
-//     }
-
-//     getDisplayText() {
-//         return 'Example view'
-//     }
-
-//     override async onOpen() {
-//         const container = this.containerEl.children[1]
-//         container?.empty()
-//         container?.createEl('h4', { text: 'Example view' })
-//     }
-
-//     override async onClose() {
-//         // Nothing to clean up.
-//     }
-// }
