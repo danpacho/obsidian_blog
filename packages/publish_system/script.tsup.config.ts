fef58b2cf6d3278e0a41eece1f17dabfaea8fb60
Danpa_cho <danpa725@cau.ac.kr>
2024-07-05 23:50:48 +0900

    chore: divide script and publish_sytem build config and adjust to scripts

---
+++ packages/publish_system/script.tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+    entry: {
+        publish_test: './scripts/publish_test.ts',
+    },
+    tsconfig: './scripts/tsconfig.json',
+    watch: false,
+    splitting: false,
+    sourcemap: false,
+    clean: false,
+    shims: true,
+    format: ['esm'],
+    target: 'esnext',
+    dts: false,
+    outDir: 'dist',
+})
