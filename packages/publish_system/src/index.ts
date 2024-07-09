ba75002ef307f5c253390a89d26394fcdac55f6b
Danpa_cho <danpa725@cau.ac.kr>
2024-07-09 17:19:30 +0900

    chore: update publish system export package annotation

--- packages/publish_system/src/index.ts
+++ packages/publish_system/src/index.ts
@@ -1,4 +1,7 @@
+// Core publish system
 export * from './publish.system'
-export * as CorePlugins from './plugin/core'
+// Plugins
 export * as Plugin from './plugin'
+export * as CorePlugins from './plugin/core'
+// Utilities
 export * as Job from './core/job.manager'
