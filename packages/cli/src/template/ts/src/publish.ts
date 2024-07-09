802b1f57712297dde9be92e4bf71acb4f6f4bab0
Danpa_cho <danpa725@cau.ac.kr>
2024-07-09 15:58:10 +0900

    feat: add base publish script template

--- packages/cli/src/template/ts/src/publish.ts
+++ packages/cli/src/template/ts/src/publish.ts
@@ -1,4 +1,49 @@
 #!/usr/bin/env node
-const publish = async () => {}
+import { CorePlugins, PublishSystem } from '@obsidian_blogger/publish_system'
+
+const publish = async () => {
+    const Publisher = new PublishSystem({
+        name: 'pub_system',
+        cwd: '{{blog_root}}',
+    })
+
+    const BlogBuilder = new CorePlugins.BlogBuilder({
+        name: 'blog_builder',
+        cwd: '{{blog_root}}',
+    })
+
+    const GithubRepository = new CorePlugins.GithubRepository({
+        name: 'github_repository',
+        cwd: '{{blog_root}}',
+        gitPath: '{{git_path}}',
+    })
+
+    Publisher.use({
+        buildScript: [BlogBuilder],
+        repository: [GithubRepository],
+    })
+
+    // Publish
+    const publishResult = await Publisher.publish<{
+        buildScript: readonly [CorePlugins.BlogBuildConfig]
+        repository: readonly [CorePlugins.GithubSaveConfig]
+        deploy?: readonly [unknown]
+    }>({
+        buildScript: [
+            {
+                buildScript: ['{{build_script}}'],
+            },
+        ],
+        repository: [
+            {
+                branch: '{{commit_branch}}',
+                commitPrefix: '{{commit_prefix}}',
+                commitMessage: '{{commit_message}}',
+            },
+        ],
+    })
+
+    return publishResult
+}
 
 publish()
