8cdef38a58834b63adff74a442f0a3e3078b6168
Danpa_cho <danpa725@cau.ac.kr>
2024-07-12 14:35:23 +0900

    chore: remove export for Job modules at publish system

--- packages/publish_system/src/index.ts
+++ packages/publish_system/src/index.ts
@@ -3,5 +3,3 @@ export * from './publish.system'
 // Plugins
 export * as Plugin from './plugin'
 export * as CorePlugins from './plugin/core'
-// Utilities
-export * as Job from './core/job.manager'
