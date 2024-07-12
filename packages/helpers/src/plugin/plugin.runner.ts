85c4c9eb39fe07573fed81de34b0037848680ddc
Danpa_cho <danpa725@cau.ac.kr>
2024-07-13 00:33:18 +0900

    refactor: PluginRunner run method interface and process

--- packages/helpers/src/plugin/plugin.runner.ts
+++ packages/helpers/src/plugin/plugin.runner.ts
@@ -1,9 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { JobManager } from '../job'
 import type { PluginInterface } from './plugin.interface'
 
 export interface PluginRunnerConstructor {
     jobManager: JobManager
 }
+
+// type PluginWithArgs<Plugin> =
+//     Plugin extends PluginInterface<any, infer Args>
+//         ? Args extends 'NO_ARGS'
+//             ? {
+//                   plugin: Plugin
+//               }
+//             : {
+//                   plugin: Plugin
+//                   args: Args
+//               }
+//         : never
+
+// type ExtractPluginArgs<Plugins extends readonly PluginInterface<any, any>[]> = {
+//     [K in keyof Plugins]: PluginWithArgs<Plugins[K]>
+// }
+
 /**
  * Manages the execution of plugin processes.
  */
@@ -20,21 +38,37 @@ export class PluginRunner {
 
     /**
      * Runs the specified plugins.
-     * @param plugins - An array of plugins to run.
+     * @param pluginPipes - Array of `plugin` and `args` to run.
      * @returns A promise that resolves to the result of plugin execution.
      */
-    public async run(plugins: Array<PluginInterface>) {
-        for (const plugin of plugins) {
+    public async run(
+        pluginPipes: Array<{
+            plugin: PluginInterface
+            args?: unknown
+        }>
+    ) {
+        for (let i = 0; i < pluginPipes.length; i++) {
+            const pluginRecord = pluginPipes[i]!
             this.$jobManager.registerJob({
-                name: plugin.config.name,
+                name: pluginRecord.plugin.config.name,
                 prepare: async () => {
-                    return await plugin.prepare?.()
+                    return await pluginRecord.plugin.prepare?.()
                 },
-                execute: async (...args) => {
-                    return await plugin.execute(...args)
+                execute: async (controller, prepared) => {
+                    const pluginExecutionContext =
+                        'args' in pluginRecord
+                            ? {
+                                  args: pluginRecord.args,
+                                  prepared,
+                              }
+                            : { prepared }
+                    return await pluginRecord.plugin.execute(
+                        pluginExecutionContext,
+                        controller
+                    )
                 },
                 cleanup: async (job) => {
-                    await plugin.cleanup?.(job)
+                    await pluginRecord.plugin.cleanup?.(job)
                     return
                 },
             })
