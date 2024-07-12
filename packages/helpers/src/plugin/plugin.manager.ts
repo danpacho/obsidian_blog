c5f9c3de66e4d6bfd62168a171a8b7e277d54d19
Danpa_cho <danpa725@cau.ac.kr>
2024-07-12 14:43:54 +0900

    feat: add integrated PluginManager class for managing plugin

--- packages/helpers/src/plugin/plugin.manager.ts
+++ packages/helpers/src/plugin/plugin.manager.ts
@@ -1,33 +1,44 @@
+import { JobManager, type JobManagerConstructor } from '../job'
 import {
     PluginConfigStore,
     type PluginConfigStoreConstructor,
 } from './plugin.config.store'
+import type { PluginInterface } from './plugin.interface'
 import { PluginLoader, type PluginLoaderConstructor } from './plugin.loader'
+import { PluginRunner } from './plugin.runner'
 
 /**
  * PluginManager constructor options.
  */
 export interface PluginManagerConstructor
     extends PluginConfigStoreConstructor,
-        PluginLoaderConstructor {}
+        PluginLoaderConstructor,
+        JobManagerConstructor {}
 
+type ExtractPluginConfig<Plugin> =
+    Plugin extends PluginInterface<infer Config> ? Config : never
 /**
  * Represents a plugin manager that handles the configuration and loading of plugins.
- *
- * @template PlugShape - The shape/interface of the plugin.
- * @template PluginConfig - The configuration type for the plugin.
+ * @template Plugin{@link PluginInterface} - The configuration type for the plugin. {@link PluginInterfaceConfig}
  */
-export class PluginManager<PlugShape, PluginConfig> {
+export class PluginManager<Plugin extends PluginInterface = PluginInterface> {
     /**
      * The configuration store for the plugins.
      */
-    public readonly $config: PluginConfigStore<PluginConfig>
+    public readonly $config: PluginConfigStore<ExtractPluginConfig<Plugin>>
 
     /**
      * The plugin loader for loading plugins.
      */
-    public readonly $loader: PluginLoader<PlugShape>
-
+    public readonly $loader: PluginLoader<Plugin>
+    /**
+     * The job manager for managing plugin execution.
+     */
+    public readonly $jobManager: JobManager
+    /**
+     * The plugin runner for running plugins.
+     */
+    private readonly $runner: PluginRunner
     /**
      * Creates a new instance of the PluginManager class.
      *
@@ -36,5 +47,19 @@ export class PluginManager<PlugShape, PluginConfig> {
     public constructor(options: PluginManagerConstructor) {
         this.$config = new PluginConfigStore(options)
         this.$loader = new PluginLoader(options)
+        this.$jobManager = new JobManager(options)
+        this.$runner = new PluginRunner({
+            jobManager: this.$jobManager,
+        })
+    }
+
+    /**
+     * Runs plugins
+     * @param except - An array of plugin names to exclude from the run.
+     * @returns A promise that resolves to the result of plugin execution.
+     */
+    public async run(except?: Array<string>) {
+        const plugins = this.$loader.load(except)
+        return await this.$runner.run(plugins)
     }
 }
