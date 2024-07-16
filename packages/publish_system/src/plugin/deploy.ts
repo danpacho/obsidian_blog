0d9705e9e3a2b6190525562da2c5f42263ca208e
Danpa_cho <danpa725@cau.ac.kr>
2024-07-16 21:38:21 +0900

    refactor: update DeployPlugin interface

--- packages/publish_system/src/plugin/deploy.ts
+++ packages/publish_system/src/plugin/deploy.ts
@@ -1,26 +1,16 @@
-import { PublishPlugin, PublishPluginConstructor } from './publish.plugin'
+import {
+    PublishPlugin,
+    type PublishPluginDynamicConfig,
+    type PublishPluginStaticConfig,
+} from './publish.plugin'
 
-export interface DeployConstructor extends PublishPluginConstructor {}
-
+export interface DeployStaticConfig extends PublishPluginStaticConfig {}
+export type DeployPluginDynamicConfig = PublishPluginDynamicConfig
 /**
  * Abstract class representing a deploy plugin.
  * Extends the PublishPlugin class.
  */
-export abstract class DeployPlugin extends PublishPlugin {
-    /**
-     * Creates a new instance of the DeployPlugin class.
-     * @param options - The options for the deploy plugin.
-     */
-    public constructor(options: DeployConstructor) {
-        super(options)
-    }
-
-    /**
-     * Abstract method for deploying the plugin.
-     * @param deployParameters - The parameters for the deployment.
-     * @returns A promise that resolves when the deployment is complete.
-     */
-    public abstract deploy(
-        deployParameters: Record<string, unknown>
-    ): Promise<unknown>
-}
+export abstract class DeployPlugin<
+    Static extends DeployStaticConfig = DeployStaticConfig,
+    Dynamic extends DeployPluginDynamicConfig = DeployPluginDynamicConfig,
+> extends PublishPlugin<Static, Dynamic> {}
