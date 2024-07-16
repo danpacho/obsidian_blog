4879195106746d9b9e1547796391fd6e0eac7bc7
Danpa_cho <danpa725@cau.ac.kr>
2024-07-16 21:21:12 +0900

    refactor: update PluginManager

--- packages/helpers/src/plugin/plugin.manager.ts
+++ packages/helpers/src/plugin/plugin.manager.ts
@@ -1,9 +1,9 @@
-import { JobManager, type JobManagerConstructor } from '../job'
+import { type JobManagerConstructor } from '../job'
 import {
     PluginConfigStore,
     type PluginConfigStoreConstructor,
 } from './plugin.config.store'
-import type { PluginInterface } from './plugin.interface'
+import { PluginShape } from './plugin.interface'
 import { PluginLoader, type PluginLoaderConstructor } from './plugin.loader'
 import { PluginRunner } from './plugin.runner'
 
@@ -13,31 +13,25 @@ import { PluginRunner } from './plugin.runner'
 export interface PluginManagerConstructor
     extends PluginConfigStoreConstructor,
         PluginLoaderConstructor,
-        JobManagerConstructor {}
-
-type ExtractPluginConfig<Plugin> =
-    Plugin extends PluginInterface<infer Config> ? Config : never
+        JobManagerConstructor {
+    runner: PluginRunner
+}
 /**
  * Represents a plugin manager that handles the configuration and loading of plugins.
- * @template Plugin{@link PluginInterface} - The configuration type for the plugin. {@link PluginInterfaceConfig}
  */
-export class PluginManager<Plugin extends PluginInterface = PluginInterface> {
+export class PluginManager<Plugin extends PluginShape = PluginShape> {
     /**
      * The configuration store for the plugins.
      */
-    public readonly $config: PluginConfigStore<ExtractPluginConfig<Plugin>>
+    public readonly $config: PluginConfigStore
     /**
      * The plugin loader for loading plugins.
      */
     public readonly $loader: PluginLoader<Plugin>
     /**
-     * The job manager for managing plugin execution.
-     */
-    public readonly $jobManager: JobManager
-    /**
      * The plugin runner for running plugins.
      */
-    private readonly $runner: PluginRunner
+    public readonly $runner: PluginRunner<Plugin>
     /**
      * Creates a new instance of the PluginManager class.
      *
@@ -46,47 +40,10 @@ export class PluginManager<Plugin extends PluginInterface = PluginInterface> {
     public constructor(options: PluginManagerConstructor) {
         this.$config = new PluginConfigStore(options)
         this.$loader = new PluginLoader(options)
-        this.$jobManager = new JobManager(options)
-        this.$runner = new PluginRunner({
-            jobManager: this.$jobManager,
-        })
+        this.$runner = options.runner
     }
 
-    /**
-     * Runs plugins
-     * @param except - An array of plugin names to exclude from the run.
-     * @returns A promise that resolves to the result of plugin execution.
-     */
-    public async run(
-        runningInformation: Array<{ name: string; args?: unknown }>
-    ): Promise<{
-        run: Awaited<ReturnType<PluginRunner['run']>>
-        save: boolean
-    }> {
-        const loadTarget = runningInformation.map((e) => e.name)
-        const pipes = this.$loader.load(loadTarget).map((plugin) => ({
-            plugin,
-            args: runningInformation.find((e) => e.name === plugin.config.name),
-        }))
-
-        const runnerResult = await this.$runner.run(pipes)
-
-        let configSaveResult: boolean = true
-        for (const pipeInfo of pipes) {
-            try {
-                await this.$config.updateConfig(pipeInfo.plugin.config.name, {
-                    config: pipeInfo.plugin
-                        .config as ExtractPluginConfig<Plugin>,
-                    args: pipeInfo.args?.args ?? null,
-                })
-            } catch {
-                configSaveResult = false
-            }
-        }
-
-        return {
-            run: runnerResult,
-            save: configSaveResult,
-        }
+    public updateRoot(root: string) {
+        this.$config.options.root = root
     }
 }
