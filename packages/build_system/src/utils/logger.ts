da0e8ba8677b6bc38205efb922f19954f86feab8
Danpa_cho <danpa725@cau.ac.kr>
2024-07-16 21:21:54 +0900

    remove: logger util

--- packages/build_system/src/utils/logger.ts
+++
@@ -1,7 +0,0 @@
-import { Logger } from '@obsidian_blogger/helpers'
-export const m = new Logger({
-    name: 'builder',
-    useDate: true,
-})
-
-export const prettyPrint = (obj: unknown) => JSON.stringify(obj, null, 2)
