e6d634d04b6e292dcff9ecf779d25f56174fe63c
Danpa_cho <danpa725@cau.ac.kr>
2024-07-16 21:11:16 +0900

    test: update plugin interface test suites

--- packages/helpers/src/plugin/plugin.interface.test.ts
+++ packages/helpers/src/plugin/plugin.interface.test.ts
@@ -1,10 +1,13 @@
 import { describe, expect, it } from 'vitest'
-import { PluginInterface } from './plugin.interface'
+import {
+    PluginInterface,
+    PluginInterfaceStaticConfig,
+} from './plugin.interface'
 
 describe('PluginInterface', () => {
     it('should create a new instance of the plugin', () => {
         class TestPlugin extends PluginInterface {
-            protected defineConfig() {
+            protected defineStaticConfig() {
                 return {
                     name: 'TestPlugin',
                     description: 'A test plugin',
@@ -16,7 +19,7 @@ describe('PluginInterface', () => {
             }
         }
         const plugin = new TestPlugin()
-        expect(plugin.config).toEqual({
+        expect(plugin.staticConfig).toEqual({
             name: 'TestPlugin',
             description: 'A test plugin',
         })
@@ -31,7 +34,7 @@ describe('PluginInterface', () => {
         } catch (e) {
             if (e instanceof Error) {
                 expect(e).toBeInstanceOf(SyntaxError)
-                expect(e.message).toContain('this.defineConfig')
+                expect(e.message).toContain('this.defineStaticConfig')
             }
         }
     })
@@ -40,7 +43,7 @@ describe('PluginInterface', () => {
         class Invalid extends PluginInterface {
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-expect-error
-            protected defineConfig() {
+            protected defineStaticConfig() {
                 return 1
             }
 
@@ -56,5 +59,134 @@ describe('PluginInterface', () => {
                 expect(e.message).toContain('config: 1')
             }
         }
+    })
+
+    it('should null when accessing dynamicConfig before setting it', () => {
+        class TestPlugin extends PluginInterface {
+            protected defineStaticConfig() {
+                return {
+                    name: 'TestPlugin',
+                    description: 'A test plugin',
+                }
+            }
+
+            public async execute(): Promise<void> {
+                return
+            }
+        }
+        const plugin = new TestPlugin()
+        expect(plugin.dynamicConfig).toBeNull()
+    })
+
+    it('should set dynamicConfig', () => {
+        class TestPlugin extends PluginInterface<
+            PluginInterfaceStaticConfig,
+            {
+                someConfig: string
+            }
+        > {
+            protected defineStaticConfig(): PluginInterfaceStaticConfig {
+                return {
+                    name: 'TestPlugin',
+                    description: 'A test plugin',
+                }
+            }
+
+            public async execute(): Promise<void> {
+                return
+            }
+        }
+        const plugin = new TestPlugin()
+        plugin.injectDynamicConfig({ someConfig: 'test' })
+        expect(plugin.dynamicConfig).toEqual({ someConfig: 'test' })
+    })
+
+    it('should correctly generate default dynamicConfigDescriptions, if it is not provided', () => {
+        class TestPlugin extends PluginInterface<
+            PluginInterfaceStaticConfig,
+            {
+                stringValue: string
+                numberValue: number
+                booleanValue: boolean
+                nullValue: null
+                functionValue: (x: number, y: number) => number
+                objectValue: {
+                    nestedString: string
+                    nestedArray: (string | number | boolean)[]
+                    k: Array<{
+                        a: string
+                        b: number
+                        c: Array<{
+                            d: boolean
+                            e: {
+                                f: Array<string>
+                                g: {
+                                    h: Array<number>
+                                }
+                            }
+                        }>
+                    }>
+                }
+                arrayValue: (string | number | boolean)[]
+            }
+        > {
+            protected defineStaticConfig(): PluginInterfaceStaticConfig {
+                return {
+                    name: 'TestPlugin',
+                    description: 'A test plugin',
+                }
+            }
+
+            public async execute(): Promise<void> {
+                return
+            }
+        }
+        const test = {
+            stringValue: 'hello',
+            numberValue: 42,
+            booleanValue: true,
+            nullValue: null,
+            functionValue: (x: number, y: number) => x + y,
+            objectValue: {
+                nestedString: 'world',
+                nestedArray: [1, '2', false],
+                k: [
+                    {
+                        a: 'a',
+                        b: 1,
+                        c: [
+                            {
+                                d: true,
+                                e: {
+                                    f: ['f'],
+                                    g: {
+                                        h: [1],
+                                    },
+                                },
+                            },
+                        ],
+                    },
+                ],
+            },
+            arrayValue: [1, 2, 3],
+        }
+        const plugin = new TestPlugin()
+        plugin.injectDynamicConfig(test)
+        expect(plugin.staticConfig).toStrictEqual({
+            name: 'TestPlugin',
+            description: 'A test plugin',
+            dynamicConfigDescriptions: [
+                { property: 'stringValue', type: 'string' },
+                { property: 'numberValue', type: 'number' },
+                { property: 'booleanValue', type: 'boolean' },
+                { property: 'nullValue', type: 'null' },
+                { property: 'functionValue', type: '(x, y) => unknown' },
+                {
+                    property: 'objectValue',
+                    type: '{ nestedString: string; nestedArray: readonly [number, string, boolean]; k: Array<{ a: string; b: number; c: Array<{ d: boolean; e: { f: Array<string>; g: { h: Array<number> } } }> }> }',
+                },
+                { property: 'arrayValue', type: 'Array<number>' },
+            ],
+        })
     })
 })
