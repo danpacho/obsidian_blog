18717ae03218ea55c866decfd7888336a6e7588f
Danpa_cho <danpa725@cau.ac.kr>
2024-07-16 21:15:44 +0900

    refactor: update PluginLoader load information added

--- packages/helpers/src/plugin/plugin.loader.ts
+++ packages/helpers/src/plugin/plugin.loader.ts
@@ -1,21 +1,56 @@
-import type { PluginInterface } from './plugin.interface'
+import type {
+    PluginInterface,
+    PluginInterfaceDynamicConfig,
+    PluginShape,
+} from './plugin.interface'
 
 export interface PluginLoaderConstructor {}
 
 /**
+ * Loading information for plugins.
+ */
+export type PluginLoadInformation = Array<{
+    /**
+     * Name of the plugin, should be unique
+     */
+    name: string
+    /**
+     * Dynamic configuration for the plugin, injected from the `obsidian`
+     */
+    dynamicConfig?: PluginInterfaceDynamicConfig
+}>
+
+type PluginKey = PluginInterface['name']
+/**
  * Represents a PluginLoader that can register and manage plugins.
  * @template Plugin The shape/interface of the plugins.
  */
-export class PluginLoader<Plugin extends PluginInterface = PluginInterface> {
+export class PluginLoader<Plugin extends PluginShape = PluginShape> {
     /**
      * Creates a new instance of PluginLoader.
-     * @param config Optional configuration for the PluginLoader.
+     * @param options Optional configuration for the PluginLoader.
      */
-    public constructor(public readonly config?: PluginLoaderConstructor) {}
+    public constructor(public readonly options?: PluginLoaderConstructor) {}
 
-    private readonly _pluginSet: Set<Plugin> = new Set<Plugin>()
+    private readonly _pluginMap: Map<PluginKey, Plugin> = new Map<
+        PluginKey,
+        Plugin
+    >()
+    public get pluginMap(): Map<PluginKey, Plugin> {
+        return this._pluginMap
+    }
+
     private _pluginList: Array<Plugin> = []
+    public get pluginList(): Array<Plugin> {
+        return this._pluginList
+    }
 
+    private register(plugin: Plugin): void {
+        if (this._pluginMap.has(plugin.name)) return
+
+        this._pluginMap.set(plugin.name, plugin)
+        this._pluginList.push(plugin)
+    }
     /**
      * Register a plugin or an array of plugins.
      * @param plugin The plugin(s) to register.
@@ -25,13 +60,11 @@ export class PluginLoader<Plugin extends PluginInterface = PluginInterface> {
         if (!plugin) return this
 
         if (Array.isArray(plugin)) {
-            plugin.forEach((p) => this._pluginSet.add(p))
-            this._pluginList = Array.from(this._pluginSet)
+            plugin.forEach((plugin) => this.register(plugin))
             return this
         }
 
-        this._pluginSet.add(plugin)
-        this._pluginList = Array.from(this._pluginSet)
+        this.register(plugin)
         return this
     }
 
@@ -39,17 +72,46 @@ export class PluginLoader<Plugin extends PluginInterface = PluginInterface> {
      * Get the list of registered plugins.
      */
     public getPluginNames(): Array<string> {
-        return this._pluginList.map((p) => p.config.name)
+        return this._pluginList.map((p) => p.staticConfig.name)
     }
 
     /**
      * Load the list of registered plugins.
-     * @param include An array of plugin names to include.
-     * @returns An array of registered plugins.
+     * @param loadInformation The information to load the plugins.
+     * @returns The loaded plugins.
      */
-    public load(include?: Array<string>): Array<Plugin> {
-        if (!include) return this._pluginList
+    public load(
+        loadInformation: Array<{
+            /**
+             * Name of the plugin, should be unique
+             */
+            name: string
+            /**
+             * Dynamic configuration for the plugin, injected from the `obsidian`
+             */
+            dynamicConfig?: PluginInterfaceDynamicConfig
+        }>
+    ): Array<Plugin> {
+        const loaded: Set<PluginKey> = new Set<PluginKey>()
+        const plugins: Array<Plugin> = loadInformation
+            .map((info) => {
+                if (loaded.has(info.name)) return undefined
 
-        return this._pluginList.filter((p) => include.includes(p.config.name))
+                const plugin: Plugin | undefined = this._pluginMap.get(
+                    info.name
+                )
+                loaded.add(info.name)
+
+                if (!plugin)
+                    throw new Error(`Plugin with name ${info.name} not found`)
+
+                if (info.dynamicConfig) {
+                    plugin.injectDynamicConfig(info.dynamicConfig)
+                }
+                return plugin
+            })
+            .filter((p) => p !== undefined)
+
+        return plugins
     }
 }
