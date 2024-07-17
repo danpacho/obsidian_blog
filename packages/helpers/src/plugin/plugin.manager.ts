9433b775c85bd2ba3ae50b0538b88a3a95871032
Danpa_cho <danpa725@cau.ac.kr>
2024-07-17 22:31:03 +0900

    refactor: add `Runner` generic type for PluginManager class

--- packages/helpers/src/plugin/plugin.manager.ts
+++ packages/helpers/src/plugin/plugin.manager.ts
@@ -3,23 +3,29 @@ import {
     PluginConfigStore,
     type PluginConfigStoreConstructor,
 } from './plugin.config.store'
-import { PluginShape } from './plugin.interface'
+import type {
+    PluginInterfaceDependencies,
+    PluginShape,
+} from './plugin.interface'
 import { PluginLoader, type PluginLoaderConstructor } from './plugin.loader'
-import { PluginRunner } from './plugin.runner'
+import type { PluginRunner } from './plugin.runner'
 
 /**
  * PluginManager constructor options.
  */
-export interface PluginManagerConstructor
+export interface PluginManagerConstructor<Runner extends PluginRunner>
     extends PluginConfigStoreConstructor,
         PluginLoaderConstructor,
         JobManagerConstructor {
-    runner: PluginRunner
+    runner: Runner
 }
 /**
  * Represents a plugin manager that handles the configuration and loading of plugins.
  */
-export class PluginManager<Plugin extends PluginShape = PluginShape> {
+export class PluginManager<
+    Plugin extends PluginShape,
+    Runner extends PluginRunner<Plugin, PluginInterfaceDependencies | null>,
+> {
     /**
      * The configuration store for the plugins.
      */
@@ -31,13 +37,13 @@ export class PluginManager<Plugin extends PluginShape = PluginShape> {
     /**
      * The plugin runner for running plugins.
      */
-    public readonly $runner: PluginRunner<Plugin>
+    public readonly $runner: Runner
     /**
      * Creates a new instance of the PluginManager class.
      *
      * @param options - Optional options for configuring the plugin manager.
      */
-    public constructor(options: PluginManagerConstructor) {
+    public constructor(options: PluginManagerConstructor<Runner>) {
         this.$config = new PluginConfigStore(options)
         this.$loader = new PluginLoader(options)
         this.$runner = options.runner
