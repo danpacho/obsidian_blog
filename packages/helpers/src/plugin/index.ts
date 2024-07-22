d7cf47f4ddc4b73da722dcd40eb15627e091ca3c
Danpa_cho <danpa725@cau.ac.kr>
2024-07-23 00:20:31 +0900

    chore: export Bridge modules as namespace

--- packages/helpers/src/plugin/index.ts
+++ packages/helpers/src/plugin/index.ts
@@ -1,5 +1,6 @@
 export * from './plugin.manager'
 export * from './plugin.interface'
+export * as Bridge from './bridge'
 export * as Config from './plugin.config.store'
 export * as Loader from './plugin.loader'
 export * as Runner from './plugin.runner'
