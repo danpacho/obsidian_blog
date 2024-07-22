e342185fbf6dd187dcc3c00dd1afa8f2b342427a
Danpa_cho <danpa725@cau.ac.kr>
2024-07-23 00:27:40 +0900

    refactor: adapt Bridge store modules at BuildSystem and update constructor options

--- packages/build_system/src/build.system.ts
+++ packages/build_system/src/build.system.ts
@@ -1,12 +1,14 @@
 import { IO, Logger } from '@obsidian_blogger/helpers'
+import { Bridge } from '@obsidian_blogger/helpers/plugin'
 import { Builder, type BuilderConstructor } from './builder'
-import type { BuildSystemAdapter } from './builder/plugin'
+import type { BuildSystemPluginAdapter } from './builder/plugin'
 import { FileTreeParser, type FileTreeParserConstructor } from './parser'
 
 //TODO: refactor, divide constructor <-> config options
 type ClassInstance = 'io' | 'logger' | 'parser'
 
 export interface BuildSystemConstructor {
+    bridgeRoot: string
     /**
      * Tree parser required constructor options
      */
@@ -14,11 +16,19 @@ export interface BuildSystemConstructor {
     /**
      * Builder required constructor options
      */
-    builder: Omit<BuilderConstructor, ClassInstance>
+    builder: Omit<
+        BuilderConstructor,
+        ClassInstance | 'bridgeRoot' | 'storagePrefix'
+    >
 }
 export class BuildSystem {
     private readonly $parser: FileTreeParser
     private readonly $builder: Builder
+    private readonly $configBridgeStore: Bridge.LoadConfigBridgeStore
+    public readonly $historyBridgeStore: Bridge.HistoryBridgeStorage
+
+    public static bridgeStorePrefix = '.store/build' as const
+
     private readonly $logger: Logger
     private readonly $io: IO
 
@@ -32,29 +42,74 @@ export class BuildSystem {
             ...options.parser,
         })
         this.$builder = new Builder({
+            ...options.builder,
             io: this.$io,
             parser: this.$parser,
             logger: this.$logger,
-            ...options.builder,
+            storagePrefix: BuildSystem.bridgeStorePrefix,
+            bridgeRoot: options.bridgeRoot,
+        })
+        this.$configBridgeStore = new Bridge.LoadConfigBridgeStore({
+            bridgeRoot: options.bridgeRoot,
+            storePrefix: BuildSystem.bridgeStorePrefix,
+            managers: [
+                this.$builder.$buildContentsPluginManager,
+                this.$builder.$treeBuildPluginManager,
+                this.$builder.$treeWalkPluginManager,
+            ],
+        })
+        this.$historyBridgeStore = new Bridge.HistoryBridgeStorage({
+            bridgeRoot: options.bridgeRoot,
+            storePrefix: BuildSystem.bridgeStorePrefix,
+            managers: [
+                this.$builder.$builderInternalPluginManager,
+                this.$builder.$buildContentsPluginManager,
+                this.$builder.$treeBuildPluginManager,
+                this.$builder.$treeWalkPluginManager,
+            ],
         })
     }
 
     /**
-     * Build the project
-     * @returns Build result
+     * Build
      */
     public async build() {
-        const response = await this.$builder.build({
-            walkTreePluginLoadInformation: [],
-            buildContentsPluginLoadInformation: [],
-            buildTreePluginLoadInformation: [],
+        await this.$configBridgeStore.init()
+        await this.$historyBridgeStore.init()
+
+        const [buildTree, walkTree, buildContents] = await Promise.all([
+            this.$configBridgeStore.loadInformation(
+                this.$builder.$treeBuildPluginManager.options.name
+            ),
+            this.$configBridgeStore.loadInformation(
+                this.$builder.$treeWalkPluginManager.options.name
+            ),
+            this.$configBridgeStore.loadInformation(
+                this.$builder.$buildContentsPluginManager.options.name
+            ),
+        ])
+
+        Bridge.LoadConfigBridgeStore.logPluginPipelineInfo({
+            [this.$builder.$builderInternalPluginManager.options.name]:
+                this.$builder.$builderInternalPluginManager.$loader.pluginList,
+            [this.$builder.$treeBuildPluginManager.options.name]:
+                this.$builder.$treeBuildPluginManager.$loader.pluginList,
+            [this.$builder.$treeWalkPluginManager.options.name]:
+                this.$builder.$treeWalkPluginManager.$loader.pluginList,
+            [this.$builder.$buildContentsPluginManager.options.name]:
+                this.$builder.$buildContentsPluginManager.$loader.pluginList,
         })
-        return response
+
+        await this.$builder.build({
+            walkTree,
+            buildContents,
+            buildTree,
+        })
     }
 
     /**
      * Register build system plugins
-     * @param plugins {@link BuildSystemAdapter}
+     * @param plugins {@link BuildSystemPluginAdapter}
      * @example
      * ```ts
      * const system = new BuildSystem(...)
@@ -65,7 +120,7 @@ export class BuildSystem {
      * })
      * ```
      */
-    public use(plugins: BuildSystemAdapter) {
+    public use(plugins: BuildSystemPluginAdapter) {
         this.$builder.use(plugins)
     }
 }
