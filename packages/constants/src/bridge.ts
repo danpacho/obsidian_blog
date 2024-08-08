fd305c828b26dc9f4929396eb3bf460d0c1c1b91
Danpa_cho <danpa725@cau.ac.kr>
2024-08-08 15:32:07 +0900

    feat: add unique manager related symbol constants

---
+++ packages/constants/src/bridge.ts
@@ -0,0 +1,38 @@
+import { type PluginDynamicConfigPrimitiveType } from '@obsidian_blogger/helpers/arg_parser'
+
+const DIVIDER = '::' as const
+
+const PREFIX = {
+    buildSystem: `build_system${DIVIDER}`,
+    publishSystem: `publish_system${DIVIDER}`,
+} as const
+
+/**
+ * Manager names, bridge store names
+ */
+export const MANAGER_NAME = {
+    buildSystem: {
+        buildTree: `${PREFIX.buildSystem}build_tree`,
+        walkTree: `${PREFIX.buildSystem}walk_tree`,
+        buildContents: `${PREFIX.buildSystem}build_contents`,
+    },
+    publishSystem: {
+        buildScript: `${PREFIX.publishSystem}build_script`,
+        repository: `${PREFIX.publishSystem}repository`,
+        deploy: `${PREFIX.publishSystem}deploy`,
+    },
+} as const
+
+const STORE_FOLDER = '.store' as const
+export const STORE_PREFIX = {
+    buildSystem: `${STORE_FOLDER}/build`,
+    publishSystem: `${STORE_FOLDER}/publish`,
+} as const
+
+export type USER_PLUGIN_LOAD_STATUS_KEY = '$$load_status$$'
+export type USER_PLUGIN_LOAD_STATUS_VALUE = 'include' | 'exclude'
+export type LOAD_STATUS = {
+    [key in USER_PLUGIN_LOAD_STATUS_KEY]: USER_PLUGIN_LOAD_STATUS_VALUE
+}
+export type USER_PLUGIN_LOAD_INPUT = LOAD_STATUS &
+    Record<string, PluginDynamicConfigPrimitiveType>
