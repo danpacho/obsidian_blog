b3eeee25001d3346a8937b749ed6f74ba9c76f24
Danpa_cho <danpa725@cau.ac.kr>
2024-08-09 19:44:28 +0900

    test: add plugin config storage test suites

---
+++ packages/plugin/src/core/plugin.config.storage.test.ts
@@ -0,0 +1,33 @@
+import { describe, expect, it } from 'vitest'
+import { PluginConfigStorage } from './plugin.config.storage'
+
+describe('PluginConfigStorage', async () => {
+    const store = new PluginConfigStorage({
+        name: 'build_system::build_contents',
+        root: `${process.cwd()}/packages/plugin/src/core/__fixtures__/store/build/build_system::walk_tree.json`,
+    })
+
+    await store.init()
+    await store.load()
+
+    it('should create a new instance of PluginConfigStorage', async () => {
+        await store.updateAllDynamicConfigByUserConfig({
+            'meta-validator': {
+                newConfig: (a, b) => a + b,
+                $$load_status$$: 'include',
+            },
+            'meta-builder': {
+                $$load_status$$: 'exclude',
+            },
+        })
+
+        const res = store.storageRecord
+
+        expect(res['meta-validator']!.dynamicConfig!.$$load_status$$).toBe(
+            'include'
+        )
+        expect(res['meta-builder']!.dynamicConfig!.$$load_status$$).toBe(
+            'exclude'
+        )
+    })
+})
