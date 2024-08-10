ff87c9e628104e38d6248ad88825c0aae934cbd6
Danpa_cho <danpa725@cau.ac.kr>
2024-08-10 22:24:08 +0900

    refactor: build, publish scripts dividing module and script

--- packages/cli/src/template/ts/src/publish.ts
+++ packages/cli/src/template/ts/src/publish.ts
@@ -1,22 +1,7 @@
 #!/usr/bin/env node
-import { CorePlugins, PublishSystem } from '@obsidian_blogger/publish_system'
-
-const Publisher = new PublishSystem({
-    bridgeRoot: '{{bridge_root}}',
-})
+import { Publisher } from './publish_core.js'
 
 const publish = async () => {
-    Publisher.use({
-        buildScript: [
-            // CorePlugins. you can remove it and modify the plugins
-            new CorePlugins.BlogBuilder(),
-        ],
-        repository: [
-            // CorePlugins. you can remove it and modify the plugins
-            new CorePlugins.GithubRepository(),
-        ],
-    })
-
     await Publisher.init()
     await Publisher.publish()
 }
---
+++ packages/cli/src/template/ts/src/publish_core.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import { CorePlugins, PublishSystem } from '@obsidian_blogger/publish_system'
+
+export const Publisher = new PublishSystem({
+    bridgeRoot: '{{bridge_root}}',
+})
+    //==============================================================================
+    //                             Plugin Registration                            //
+    //==============================================================================
+    .use({
+        buildScript: [
+            // CorePlugins. you can remove it and modify the plugins
+            new CorePlugins.BlogBuilder(),
+        ],
+        repository: [
+            // CorePlugins. you can remove it and modify the plugins
+            new CorePlugins.GithubRepository(),
+        ],
+    })
