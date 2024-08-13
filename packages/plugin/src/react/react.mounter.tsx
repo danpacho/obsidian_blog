dce85286efec602ec1ca13e784ba2e1c590ecd23
Danpa_cho <danpa725@cau.ac.kr>
2024-08-14 00:06:42 +0900

    feat: render view using react mounter

--- packages/plugin/src/react/react.mounter.tsx
+++ packages/plugin/src/react/react.mounter.tsx
@@ -2,23 +2,53 @@ import { ItemView, WorkspaceLeaf } from 'obsidian'
 import { StrictMode } from 'react'
 import { Root, createRoot } from 'react-dom/client'
 import { AppContext } from './provider/app.root'
-import { BuildView } from './view/build.view'
+import { BuildView } from './view/build/build.view'
+import { SetupView } from './view/setup/setup.view'
+import ObsidianBlogger from '~/plugin/main'
+import { io } from '~/utils/io'
 
 export const VIEW_TYPE = '$ROOT_VIEW$' as const
 
 export class ReactView extends ItemView {
     private root: Root | null = null
-
-    public constructor(leaf: WorkspaceLeaf) {
-        super(leaf)
+    private readonly _plugin: ObsidianBlogger
+    public constructor(viewProps: {
+        leaf: WorkspaceLeaf
+        plugin: ObsidianBlogger
+    }) {
+        super(viewProps.leaf)
+        this._plugin = viewProps.plugin
     }
+
+    private _state: 'setup_view' | 'build_view' = 'setup_view'
 
     public getViewType() {
         return VIEW_TYPE
     }
 
     public getDisplayText() {
-        return 'obsidian-blogger'
+        return 'obsidian-blogger' as const
+    }
+
+    override onload(): void {
+        this._state = io.fileExistsSync(
+            this._plugin.settings.bridge_install_root
+        )
+            ? 'build_view'
+            : 'setup_view'
+        this._state = 'setup_view'
+    }
+
+    private render(): void {
+        this.root = createRoot(this.containerEl.children[1]!)
+        this.root.render(
+            <StrictMode>
+                <AppContext.Provider value={this._plugin}>
+                    {this._state === 'setup_view' ? <SetupView /> : null}
+                    {this._state === 'build_view' ? <BuildView /> : null}
+                </AppContext.Provider>
+            </StrictMode>
+        )
     }
 
     override async onOpen() {
@@ -26,14 +56,8 @@ export class ReactView extends ItemView {
         if (!isTargetRootExist) {
             this.containerEl.createDiv({ cls: 'react-root' })
         }
-        this.root = createRoot(this.containerEl.children[1]!)
-        this.root.render(
-            <StrictMode>
-                <AppContext.Provider value={this.app}>
-                    <BuildView />
-                </AppContext.Provider>
-            </StrictMode>
-        )
+
+        this.render()
     }
 
     override async onClose() {
