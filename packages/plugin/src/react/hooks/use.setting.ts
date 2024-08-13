9fc20b90b76aab00addb1b788504c94d9c7154d3
Danpa_cho <danpa725@cau.ac.kr>
2024-08-14 00:05:03 +0900

    refactor: add loaded property at obsidian setting loader hook

--- packages/plugin/src/react/hooks/use.setting.ts
+++ packages/plugin/src/react/hooks/use.setting.ts
@@ -2,20 +2,30 @@ import { useEffect, useState } from 'react'
 import { useApp } from '../provider/app.root'
 import { ObsidianBloggerSettings } from '~/plugin/settings'
 
-export const useObsidianSetting = (): ObsidianBloggerSettings | undefined => {
+export const useObsidianSetting = (
+    defaultSetting?: ObsidianBloggerSettings
+): {
+    settings: ObsidianBloggerSettings | undefined
+    loaded: boolean
+} => {
     const app = useApp()
 
+    const [loaded, setLoaded] = useState<boolean>(false)
     const [settings, setSettings] = useState<
         ObsidianBloggerSettings | undefined
-    >(undefined)
+    >(defaultSetting)
 
     useEffect(() => {
         const loadSettings = async () => {
             const settings = await app?.loadData()
-            setSettings(settings)
+            const mergedSettings = defaultSetting
+                ? { ...defaultSetting, ...settings }
+                : settings
+            setSettings(mergedSettings)
+            setLoaded(true)
         }
         loadSettings()
     }, [app])
 
-    return settings
+    return { settings, loaded }
 }
