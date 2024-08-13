9129c6add3e9eb64156b405cc1e741d2d2f7d3b5
Danpa_cho <danpa725@cau.ac.kr>
2024-08-13 19:33:03 +0900

    feat: add input common component

---
+++ packages/plugin/src/react/common/input.tsx
@@ -0,0 +1,49 @@
+export interface InputProps {
+    title: string
+    description?: string | React.ReactNode
+    input: string
+    setInput: (input: string) => void
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+export const Input = ({
+    children,
+    title,
+    description,
+    input,
+    setInput,
+    onChange,
+}: React.PropsWithChildren<InputProps>) => {
+    const isDescriptionString = typeof description === 'string'
+    return (
+        <div className="flex w-full flex-col items-start justify-between gap-y-1">
+            <div className="flex flex-row items-center justify-between gap-x-1">
+                <h1 className="ml-1 text-sm font-light text-stone-300">
+                    {title}
+                </h1>
+                {isDescriptionString && (
+                    <p className="ml-1 text-xs font-light text-stone-400">
+                        {description}
+                    </p>
+                )}
+                {!isDescriptionString && description && (
+                    <div className="ml-1">{description}</div>
+                )}
+            </div>
+
+            <input
+                className="!w-full text-sm font-normal !text-stone-300 placeholder:!text-stone-500"
+                onChange={(e) => {
+                    setInput(e.target.value)
+                    onChange?.(e)
+                }}
+                placeholder={isDescriptionString ? description : title}
+                value={input}
+                type="text"
+                aria-label={title}
+                name={title}
+            >
+                {children}
+            </input>
+        </div>
+    )
+}
