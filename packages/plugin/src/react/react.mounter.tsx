252fe71dccd5324b730f13ccc00cf17189e1f42b
Danpa_cho <danpa725@cau.ac.kr>
2024-08-14 16:47:46 +0900

    refactor: extract initial routing logic into App core component, and update render method

--- packages/plugin/src/react/react.mounter.tsx
+++ packages/plugin/src/react/react.mounter.tsx
@@ -1,11 +1,10 @@
 import { ItemView, WorkspaceLeaf } from 'obsidian'
 import { StrictMode } from 'react'
 import { Root, createRoot } from 'react-dom/client'
+import { App } from './app'
 import { AppContext } from './provider/app.root'
-import { BuildView } from './view/build/build.view'
-import { SetupView } from './view/setup/setup.view'
+import { Routing } from './routing'
 import ObsidianBlogger from '~/plugin/main'
-import { io } from '~/utils/io'
 
 export const VIEW_TYPE = '$ROOT_VIEW$' as const
 
@@ -20,8 +19,6 @@ export class ReactView extends ItemView {
         this._plugin = viewProps.plugin
     }
 
-    private _state: 'setup_view' | 'build_view' = 'setup_view'
-
     public getViewType() {
         return VIEW_TYPE
     }
@@ -30,23 +27,17 @@ export class ReactView extends ItemView {
         return 'obsidian-blogger' as const
     }
 
-    override onload(): void {
-        this._state = io.fileExistsSync(
-            this._plugin.settings.bridge_install_root
-        )
-            ? 'build_view'
-            : 'setup_view'
-        this._state = 'setup_view'
-    }
+    override onload(): void {}
 
     private render(): void {
         this.root = createRoot(this.containerEl.children[1]!)
         this.root.render(
             <StrictMode>
-                <AppContext.Provider value={this._plugin}>
-                    {this._state === 'setup_view' ? <SetupView /> : null}
-                    {this._state === 'build_view' ? <BuildView /> : null}
-                </AppContext.Provider>
+                <Routing.Provider>
+                    <AppContext.Provider value={this._plugin}>
+                        <App />
+                    </AppContext.Provider>
+                </Routing.Provider>
             </StrictMode>
         )
     }
