0800e53966d2290b89b848b44a8f139d4c025bf0
Danpa_cho <danpa725@cau.ac.kr>
2024-08-16 17:09:50 +0900

    feat: add multiple input handler hook and generalize use.input hook

--- packages/plugin/src/react/hooks/use.input.ts
+++ packages/plugin/src/react/hooks/use.input.ts
@@ -1,10 +1,41 @@
 import { useState } from 'react'
 
-export const useInput = (defaultValue: string = '') => {
-    const [input, setInput] = useState<string>(defaultValue)
+export const useInput = <InputType = string>(defaultValue: InputType) => {
+    const [input, setInput] = useState<InputType>(defaultValue)
     return {
         input,
         setInput,
         defaultValue,
     }
+}
+
+export const useMultipleInput = <
+    const InputRecord extends Record<string, unknown>,
+>(
+    inputRecord: InputRecord
+): readonly [
+    Map<keyof InputRecord, unknown>,
+    {
+        setInput: (key: string, value: unknown) => void
+        getInput: (key: string) => unknown
+    },
+] => {
+    const [inputs, setInputs] = useState<Map<string, unknown>>(
+        () =>
+            new Map(
+                Object.entries(inputRecord).map(([key, value]) => [
+                    key,
+                    value ?? '',
+                ])
+            )
+    )
+    const getInput = (key: string): unknown => inputs.get(key) ?? 'NULL'
+    const setInput = (key: string, value: unknown) => {
+        setInputs((input) => {
+            const newState = new Map(input)
+            newState.set(key, value)
+            return newState
+        })
+    }
+    return [inputs, { setInput, getInput }]
 }
