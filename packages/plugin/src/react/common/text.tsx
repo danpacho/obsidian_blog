44761fafdd6f8eb992379a4e48f3308dd1bc38cc
Danpa_cho <danpa725@cau.ac.kr>
2024-08-16 17:10:59 +0900

    feat: add text common component

---
+++ packages/plugin/src/react/common/text.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { tw } from '../tw'
+import { type TailwindComponent } from './tailwind.component'
+
+const header = tw.style({
+    fontFamily: 'font-mono',
+    fontSize: 'text-lg',
+    fontWeight: 'font-semibold',
+    color: 'text-stone-300',
+})
+const Header = (props: React.PropsWithChildren<TailwindComponent>) => {
+    const className = props.tw
+        ? tw.mergeProps(header.style, props.tw)
+        : header.class
+    return <h1 className={className}>{props.children}</h1>
+}
+const subHeader = tw.style({
+    fontFamily: 'font-mono',
+    fontSize: 'text-base',
+    fontWeight: 'font-semibold',
+    color: 'text-stone-300',
+})
+const SubHeader = (props: React.PropsWithChildren<TailwindComponent>) => {
+    const className = props.tw
+        ? tw.mergeProps(subHeader.style, props.tw)
+        : subHeader.class
+    return <h2 className={className}>{props.children}</h2>
+}
+const description = tw.style({
+    fontFamily: 'font-sans',
+    fontSize: 'text-sm',
+    fontWeight: 'font-light',
+    color: 'text-stone-400',
+})
+const Description = (props: React.PropsWithChildren<TailwindComponent>) => {
+    const className = props.tw
+        ? tw.mergeProps(description.style, props.tw)
+        : description.class
+    return <p className={className}>{props.children}</p>
+}
+const paragraph = tw.style({
+    fontFamily: 'font-sans',
+    fontSize: 'text-base',
+    fontWeight: 'font-normal',
+    color: 'text-stone-300',
+})
+const Paragraph = (props: React.PropsWithChildren<TailwindComponent>) => {
+    const className = props.tw
+        ? tw.mergeProps(paragraph.style, props.tw)
+        : paragraph.class
+    return <p className={className}>{props.children}</p>
+}
+
+export const Text = {
+    Header,
+    SubHeader,
+    Description,
+    Paragraph,
+}
