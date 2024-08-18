2d883a54472c90c7effadc9767f4185a2ea89d31
Danpa_cho <danpa725@cau.ac.kr>
2024-08-18 22:26:29 +0900

    feat: add Text.Code code related component

--- packages/plugin/src/react/common/text.tsx
+++ packages/plugin/src/react/common/text.tsx
@@ -50,10 +50,23 @@ const Paragraph = (props: React.PropsWithChildren<TailwindComponent>) => {
         : paragraph.class
     return <p className={className}>{props.children}</p>
 }
+const code = tw.style({
+    fontFamily: 'font-mono',
+    fontSize: 'text-sm/5',
+    fontWeight: 'font-normal',
+    color: 'text-stone-300',
+})
+const Code = (props: React.PropsWithChildren<TailwindComponent>) => {
+    const className = props.tw
+        ? tw.mergeProps(code.style, props.tw)
+        : code.class
+    return <pre className={className}>{props.children}</pre>
+}
 
 export const Text = {
     Header,
     SubHeader,
     Description,
     Paragraph,
+    Code,
 }
