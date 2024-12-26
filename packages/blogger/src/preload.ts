20dc8ae89fbe2ecbe77980f48f2a995a0103aaa9
Danpa_cho <danpa725@cau.ac.kr>
2024-12-26 18:14:52 +0900

    feat: add electronjs preload script

---
+++ packages/blogger/src/preload.ts
@@ -0,0 +1,6 @@
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+// import { contextBridge } from 'electron'
+
+// Node.js Tools
+// I will not use this as contextBridge, because there is so many things for node
