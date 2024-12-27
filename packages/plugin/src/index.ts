7f6944236038cf3777ca25962fbaeb6eaf93829a
Danpa_cho <danpa725@cau.ac.kr>
2024-12-28 00:25:38 +0900

    refactor: remove Config namespace at plugin pkg

--- packages/plugin/src/index.ts
+++ packages/plugin/src/index.ts
@@ -1,6 +1,5 @@
-export * from './plugin.manager'
+export * from './bridge/plugin.manager'
 export * from './plugin.interface'
 export * as Bridge from './bridge'
-export * as Config from './plugin.config.storage'
 export * as Loader from './plugin.loader'
 export * as Runner from './plugin.runner'
