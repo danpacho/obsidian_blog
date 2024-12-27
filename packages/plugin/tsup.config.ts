de30230f0ea817c5d858034c486f20a4a0983d63
Danpa_cho <danpa725@cau.ac.kr>
2024-12-28 00:23:24 +0900

    chore: update build config for plugin pkg

--- packages/plugin/tsup.config.ts
+++ packages/plugin/tsup.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'tsup'
 export default defineConfig((options) => ({
     entry: {
         index: 'src/index.ts',
-        arg_parser: 'src/plugin/arg_parser/index.ts',
-        bridge: 'src/plugin/bridge/index.ts',
+        arg_parser: 'src/arg_parser/index.ts',
+        constants: 'src/constants/index.ts',
+        bridge: 'src/bridge/index.ts',
     },
     watch: options.watch ? ['src/**/*'] : false,
     clean: false,
