e55d9c51601b7771f1556c098ae9c3f96dfa57eb
Danpa_cho <danpa725@cau.ac.kr>
2024-12-30 00:46:08 +0900

    test: update test suite for pagination

--- packages/build_system/src/builder/plugin/__tests__/static.param.builder.test.ts
+++ packages/build_system/src/builder/plugin/__tests__/static.param.builder.test.ts
@@ -6,22 +6,30 @@ describe('StaticParamBuilderPlugin', () => {
     it('should inject static params to the content', async () => {
         const plugin = new StaticParamBuilderPlugin()
         plugin.injectDynamicConfig({
-            paramShape: '/[...posts]',
+            paramShape: '/posts/$page/[...posts]',
+            maxPage: 2,
         })
 
-        const { buildFileNames, buildFiles } = await Tester.pipe({
+        const { buildFiles } = await Tester.pipe({
             plugin: {
                 'walk:tree': plugin,
             },
         })
 
         const metaList = buildFiles.contents.map((e) => e.meta!.params)
-        const expectedPostParams = buildFileNames.contents.map((e) => {
-            const param = e.split('.').slice(0, -1)[0]
-            return {
-                posts: param,
-            }
-        })
-        expect(metaList).toEqual(expectedPostParams)
+        expect(metaList).toEqual([
+            {
+                posts: 'posts/1/img',
+            },
+            {
+                posts: 'posts/1/link',
+            },
+            {
+                posts: 'posts/2/markdown',
+            },
+            {
+                posts: 'posts/2/nested/nested/nested',
+            },
+        ])
     })
 })
