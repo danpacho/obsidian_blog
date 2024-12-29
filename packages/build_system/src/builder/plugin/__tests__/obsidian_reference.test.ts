a52cce47b2e3febc96eb063b9ca71dec82ba37c7
Danpa_cho <danpa725@cau.ac.kr>
2024-12-29 16:26:30 +0900

    test: add obsidian reference plugin test

---
+++ packages/build_system/src/builder/plugin/__tests__/obsidian_reference.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, it } from 'vitest'
+import { ObsidianReferencePlugin } from '../core'
+import { Tester } from './tester'
+
+describe('ObsidianReferencePlugin', () => {
+    it('should reference obsidian images', async () => {
+        const plugin = new ObsidianReferencePlugin()
+        const res = await Tester.pipe({
+            plugin: {
+                'build:contents': plugin,
+            },
+        })
+
+        const imgFile = res.buildFiles.contents.find(
+            (e) => e.filename === 'img.md'
+        )
+        const imgFilename = res.buildFiles.assets.find((e) =>
+            e.filename.includes('.png')
+        )
+        const imgPath__replaced_origin_build_path = imgFilename?.path.replace(
+            res.buildPath.assets,
+            ''
+        )
+
+        expect(imgFile?.content).toContain(
+            `<img src="${imgPath__replaced_origin_build_path}" alt="img.png" />`
+        )
+    })
+})
