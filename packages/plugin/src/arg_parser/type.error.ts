b23e2f145c927b985f8e02ef15e95b90f303a690
Danpa_cho <danpa725@cau.ac.kr>
2024-12-31 14:52:24 +0900

    refactor: add additional error checking field at ArgTypeError

--- packages/plugin/src/arg_parser/type.error.ts
+++ packages/plugin/src/arg_parser/type.error.ts
@@ -1,12 +1,18 @@
 export class ArgTypeError extends Error {
     public constructor(
         public readonly expected: string,
-        public readonly received: unknown
+        public readonly received: unknown,
+        public readonly additionalError?: Array<Error>
     ) {
         const isRecord = (value: unknown): value is Record<string, unknown> =>
             typeof value === 'object' && !Array.isArray(value)
-        super(
-            `ArgTypeError: Expected ${expected}, received ${typeof received} › ${isRecord(received) ? JSON.stringify(received, null, 4) : received}`
-        )
+
+        const argErrorMessage = `ArgTypeError: Expected ${expected}, received ${typeof received} › ${isRecord(received) ? JSON.stringify(received, null, 4) : received}`
+        const additionalErrorMessages = additionalError
+            ?.map((error) => error.message)
+            .join('\n')
+
+        const totalMessage = `${argErrorMessage}${additionalErrorMessages ? `\nAdditionalError: ${additionalErrorMessages}` : ''}`
+        super(totalMessage)
     }
 }
