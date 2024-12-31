cf128e843b6a958a97fac2eb5d610bcd5c90e4ed
Danpa_cho <danpa725@cau.ac.kr>
2024-12-31 14:55:33 +0900

    test: add updated logic result for static param builder

--- packages/build_system/src/builder/plugin/__tests__/static.param.builder.test.ts
+++ packages/build_system/src/builder/plugin/__tests__/static.param.builder.test.ts
@@ -6,7 +6,8 @@ describe('StaticParamBuilderPlugin', () => {
     it('should inject static params to the content', async () => {
         const plugin = new StaticParamBuilderPlugin()
         plugin.injectDynamicConfig({
-            paramShape: '/posts/$page/[...posts]',
+            prefix: 'posts',
+            paramShape: '/[$page]/[...postId]',
             maxPage: 2,
         })
 
@@ -18,18 +19,18 @@ describe('StaticParamBuilderPlugin', () => {
 
         const metaList = buildFiles.contents.map((e) => e.meta!.params)
         expect(metaList).toEqual([
-            {
-                posts: 'posts/1/img',
-            },
-            {
-                posts: 'posts/1/link',
-            },
-            {
-                posts: 'posts/2/markdown',
-            },
-            {
-                posts: 'posts/2/nested/nested/nested',
-            },
+            { postId: 'img', page: '1' },
+            { postId: 'link', page: '1' },
+            { postId: 'markdown', page: '2' },
+            { postId: 'nested/nested/nested', page: '2' },
+        ])
+
+        const hrefList = buildFiles.contents.map((e) => e.meta!.href)
+        expect(hrefList).toEqual([
+            'posts/1/img',
+            'posts/1/link',
+            'posts/2/markdown',
+            'posts/2/nested/nested/nested',
         ])
     })
 })
