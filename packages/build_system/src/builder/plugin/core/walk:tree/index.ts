8754301103dbf07c41e3f9480715e26c812813d5
Danpa_cho <danpa725@cau.ac.kr>
2025-01-01 19:53:52 +0900

    feat: export meta img path matcher plugin

--- packages/build_system/src/builder/plugin/core/walk:tree/index.ts
+++ packages/build_system/src/builder/plugin/core/walk:tree/index.ts
@@ -1,6 +1,7 @@
 export * from './category.description.generator'
 export * from './meta.builder'
 export * from './meta.validator'
+export * from './meta.img.path.matcher'
 export * from './pagination.builder'
 export * from './series.info.generator'
 export * from './static.param.builder'
