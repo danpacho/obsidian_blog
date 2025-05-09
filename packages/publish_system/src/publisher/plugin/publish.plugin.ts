21cbc531e120d7706abd186df4648cb463a243f1
Danpa_cho <danpa725@cau.ac.kr>
2025-05-10 00:01:08 +0900

    refactor: publish plugin response

--- packages/publish_system/src/publisher/plugin/publish.plugin.ts
+++ packages/publish_system/src/publisher/plugin/publish.plugin.ts
@@ -1,15 +1,18 @@
 import { IO } from '@obsidian_blogger/helpers/io'
 import {
     Logger,
+    type LogHistory,
     type LoggerConstructor,
 } from '@obsidian_blogger/helpers/logger'
 import {
     ShellExecutor,
+    type CommandResult,
     type ShellExecutorConstructor,
 } from '@obsidian_blogger/helpers/shell'
 import {
     PluginInterface,
-    PluginInterfaceStaticConfig,
+    type PluginExecutionResponse,
+    type PluginInterfaceStaticConfig,
 } from '@obsidian_blogger/plugin_api'
 
 export type PublishPluginDynamicConfig = {
@@ -18,6 +21,26 @@ export type PublishPluginDynamicConfig = {
      */
     cwd: string
 }
+
+export interface PublishPluginResponse {
+    /**
+     * Error stacks
+     */
+    error: Array<{ error: Error; command?: CommandResult }>
+    /**
+     * History of log messages
+     */
+    history: Array<LogHistory>
+    /**
+     * Standard output
+     */
+    stdout?: string
+}
+export type PublishCommandResult = CommandResult
+
+export type PublishPluginExecutionResponse =
+    PluginExecutionResponse<PublishPluginResponse>
+
 export interface PublishPluginStaticConfig extends PluginInterfaceStaticConfig {
     /**
      * Shell executor options
@@ -41,7 +64,15 @@ export abstract class PublishPlugin<
     Static extends PublishPluginStaticConfig = PublishPluginStaticConfig,
     Dynamic extends PublishPluginDynamicConfig = PublishPluginDynamicConfig,
     Dependencies extends PublishPluginDependencies = PublishPluginDependencies,
-> extends PluginInterface<Static, Dynamic, Dependencies> {
+> extends PluginInterface<
+    Static,
+    Dynamic,
+    Dependencies,
+    {
+        prepare: unknown
+        response: PublishPluginResponse
+    }
+> {
     protected get $logger() {
         return this.getRunTimeDependency('logger')
     }
@@ -50,5 +81,49 @@ export abstract class PublishPlugin<
     }
     protected get $io() {
         return this.getRunTimeDependency('io')
+    }
+
+    override async prepare(): Promise<void> {
+        this.$logger.updateName(this.staticConfig.name)
+        return
+    }
+
+    protected invokeError(
+        errorStack: PublishPluginResponse['error'],
+        invoker: {
+            e: unknown
+            message: string
+            commandResult?: PublishPluginResponse['error'][number]['command']
+        }
+    ): void {
+        const { e, message, commandResult } = invoker
+        this.$logger.error(message)
+        if (e instanceof Error) {
+            errorStack.push(
+                commandResult
+                    ? {
+                          error: e,
+                          command: commandResult,
+                      }
+                    : {
+                          error: e,
+                      }
+            )
+        } else {
+            errorStack.push(
+                commandResult
+                    ? {
+                          error: new Error(message, {
+                              cause: e,
+                          }),
+                          command: commandResult,
+                      }
+                    : {
+                          error: new Error(message, {
+                              cause: e,
+                          }),
+                      }
+            )
+        }
     }
 }
