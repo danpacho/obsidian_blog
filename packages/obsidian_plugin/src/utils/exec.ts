46eaf8d330d1d02ad6d17358623595c1fc83ab49
Danpa_cho <danpa725@cau.ac.kr>
2025-05-17 19:47:16 +0900

    refactor: divide exec bridge script into utils

---
+++ packages/obsidian_plugin/src/utils/exec.ts
@@ -0,0 +1,57 @@
+import type { ObsidianBloggerSettings } from '~/plugin/settings'
+import { Shell } from '~/utils'
+
+export const BuildPlugin = async ({
+    node_bin,
+    bridge_install_root,
+}: ObsidianBloggerSettings) => {
+    if (!node_bin || !bridge_install_root) return
+
+    const buildResult = await Shell.spawn$('npm', ['run', 'build'], {
+        cwd: bridge_install_root,
+        env: {
+            PATH: `${process.env.PATH}:${node_bin}`,
+        },
+    })
+    return buildResult
+}
+
+export const InitPlugin = async ({
+    node_bin,
+    bridge_install_root,
+}: ObsidianBloggerSettings) => {
+    if (!node_bin || !bridge_install_root) return
+
+    const buildResult = await Shell.spawn$('npm', ['run', 'run:init'], {
+        cwd: bridge_install_root,
+        env: {
+            PATH: `${process.env.PATH}:${node_bin}`,
+        },
+    })
+    return buildResult
+}
+
+export const ExecutePlugin = async (
+    opt: ObsidianBloggerSettings & {
+        command: 'run:build' | 'run:publish'
+    }
+) => {
+    const { command, node_bin, bridge_install_root } = opt
+    if (!node_bin || !bridge_install_root) return
+
+    const buildResult = await BuildPlugin(opt)
+
+    const isBuildError =
+        buildResult === undefined ||
+        ('error_code' in buildResult && buildResult.stderr !== '')
+
+    if (isBuildError) return
+
+    const executionResult = await Shell.spawn$('npm', ['run', command], {
+        cwd: bridge_install_root,
+        env: {
+            PATH: `${process.env.PATH}:${node_bin}`,
+        },
+    })
+    return executionResult
+}
