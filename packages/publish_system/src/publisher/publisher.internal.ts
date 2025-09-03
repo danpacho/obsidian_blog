d69b5ccb831a6967dd708656aa3543741a520146
Danpa_cho <danpa725@cau.ac.kr>
2025-09-03 19:03:21 +0900

    refactor: core publisher runner

--- packages/publish_system/src/publisher/publisher.internal.ts
+++ packages/publish_system/src/publisher/publisher.internal.ts
@@ -9,31 +9,10 @@ export class PublishPluginRunner extends Runner.PluginRunner<
     PublishPlugin,
     PublishPluginDependencies
 > {
-    public async run(
-        pluginPipes: PublishPlugin[],
-        dependencies: PublishPluginDependencies
-    ) {
-        for (const plugin of pluginPipes) {
-            this.$pluginRunner.registerJob({
-                name: plugin.name,
-                prepare: async () => {
-                    dependencies.logger.updateName(plugin.name)
-                    plugin.injectDependencies(dependencies)
-                    return await plugin.prepare?.()
-                },
-                execute: async (controller, prepared) => {
-                    return await plugin.execute(controller, prepared)
-                },
-                cleanup: async (job) => {
-                    for (const res of job?.response ?? []) {
-                        await plugin.cleanup?.(res)
-                    }
-                },
-            })
-        }
-
-        await this.$pluginRunner.processJobs()
-
-        return this.history
+    protected override async pluginPrepare(
+        plugin: PublishPlugin,
+        runtimeDependencies: PublishPluginDependencies
+    ): Promise<void> {
+        runtimeDependencies.logger.updateName(plugin.name)
     }
 }
